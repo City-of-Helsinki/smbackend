4d3eb0f11b4fc107a15ce0c71313a9c9aee6710f
Juha Yrjölä <juha.yrjola@iki.fi>
2014-01-27 22:44:57 +0200

    Remove linguo from INSTALLED_APPS

--- smbackend/settings.py
+++ smbackend/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'tastypie',
-    'linguo',
     'corsheaders',
     'django_extensions',
     'modeltranslation',
