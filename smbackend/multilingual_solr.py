3547ffc860490e16ad2a505525428089621de88a
Juha Yrjölä <juha.yrjola@iki.fi>
2014-10-21 20:21:00 +0300

    Make Haystack remove operation use multilingual backends

--- smbackend/multilingual_solr.py
+++ smbackend/multilingual_solr.py
@@ -11,8 +11,9 @@ def get_using(language, alias=DEFAULT_ALIAS):
     using = new_using if new_using in settings.HAYSTACK_CONNECTIONS else alias
     return using
 
+
 class MultilingualSolrSearchBackend(SolrSearchBackend):
-    def update(self, index, iterable, commit=True):
+    def _multi_backend_run(self, func_name, *args, **kwargs):
         # do not allow calling update on a multilingual backend
         if re.search(r'_[\w]{2}$', self.connection_alias):
             return
@@ -29,15 +30,24 @@ class MultilingualSolrSearchBackend(SolrSearchBackend):
             translation.activate(language)
             backend = connections[using].get_backend()
             print('[%s]' % using)
-            super(MultilingualSolrSearchBackend, backend).update(index, iterable, commit)
+            func = getattr(super(MultilingualSolrSearchBackend, backend), func_name)
+            func(*args, **kwargs)
 
         translation.activate(initial_language)
+
+    def update(self, index, iterable, commit=True):
+        self._multi_backend_run('update', index, iterable, commit=commit)
+
+    def remove(self, obj_or_string, commit=True):
+        self._multi_backend_run('remove', obj_or_string, commit=commit)
+
 
 class MultilingualSolrSearchQuery(SolrSearchQuery):
     def __init__(self, using=DEFAULT_ALIAS):
         language = translation.get_language()[:2]
         using = get_using(language)
         super(MultilingualSolrSearchQuery, self).__init__(using=using)
+
 
 class MultilingualSolrEngine(SolrEngine):
     backend = MultilingualSolrSearchBackend
