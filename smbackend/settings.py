3ea01ddd3ab4dbc9e4615661befef20726d12800
Timo Tuominen <tituomin@gmail.com>
2015-12-16 11:58:27 +0200

    Start using memcached.

--- smbackend/settings.py
+++ smbackend/settings.py
@@ -70,6 +70,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 # Keep the database connection open for 120s
 CONN_MAX_AGE = 120
 
