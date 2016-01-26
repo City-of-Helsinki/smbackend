f57092e217cebbb6f5416df9c9642748369f8d0d
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-01-26 18:29:56 +0200

    Added renderers for plain text and KML
    KML renderer just renders static template
    at the moment.

--- services/api.py
+++ services/api.py
@@ -21,6 +21,10 @@ from services.accessibility import RULES as accessibility_rules
 from munigeo.models import *
 from munigeo import api as munigeo_api
 
+from rest_framework import renderers
+from rest_framework_jsonp.renderers import JSONPRenderer
+import pprint
+from django.template.loader import render_to_string
 
 # This allows us to find a serializer for Haystack search results
 serializers_by_model = {}
@@ -248,6 +252,7 @@ class ServiceViewSet(JSONAPIViewSet, viewsets.ReadOnlyModelViewSet):
 
 register_view(ServiceViewSet, 'service')
 
+
 class UnitSerializer(TranslatedModelSerializer, MPTTModelSerializer,
                      munigeo_api.GeoModelSerializer, JSONAPISerializer):
     connections = UnitConnectionSerializer(many=True)
@@ -321,9 +326,28 @@ def make_muni_ocd_id(name, rest=None):
     return s
 
 
+class PlainTextRenderer(renderers.BaseRenderer):
+    media_type = 'text/plain'
+    format = 'txt'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return pprint.pformat(data)
+
+
+class KmlRenderer(renderers.BaseRenderer):
+    media_type = 'application/xml'
+    format = 'xml'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return render_to_string('kml.xml', data)
+
+
 class UnitViewSet(munigeo_api.GeoModelAPIView, JSONAPIViewSet, viewsets.ReadOnlyModelViewSet):
     queryset = Unit.objects.all()
     serializer_class = UnitSerializer
+
+    renderer_classes = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer, JSONPRenderer,
+                        PlainTextRenderer, KmlRenderer)
 
     def get_serializer_context(self):
         ret = super(UnitViewSet, self).get_serializer_context()
