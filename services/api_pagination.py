95ab901205fbc70fd93b7613f6f05c11baf350b3
Timo Tuominen <tituomin@gmail.com>
2016-02-23 14:10:40 +0200

    Simpler KML pagination.

--- services/api_pagination.py
+++ services/api_pagination.py
@@ -8,9 +8,6 @@ class Pagination(PageNumberPagination):
     max_page_size = 1000
 
     def get_page_size(self, request):
-        stored_size = self.max_page_size
         if hasattr(request, 'accepted_media_type') and re.match(KML_RE, request.accepted_media_type):
-            self.max_page_size = 30000
-        page_size = super(Pagination, self).get_page_size(request)
-        self.max_page_size = stored_size
-        return page_size
+            return 30000
+        return super(Pagination, self).get_page_size(request)
