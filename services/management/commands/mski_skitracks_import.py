1879f76c72fb262b3e9e4ecd08e2536ddad70edc
Timo Tuominen <tituomin@gmail.com>
2016-12-01 17:36:23 +0200

    Import Espoo tracks.

--- services/management/commands/mski_skitracks_import.py
+++ services/management/commands/mski_skitracks_import.py
@@ -17,6 +17,41 @@ from django.contrib.gis.utils import LayerMapping
 
 from services.models import *
 
+def espoo_coordinates_to_gk25(x, y):
+    a = 6600290.731951121200000
+    b = 25443205.726901203000000
+    c = 0.999869662254702
+    d = -0.015128383929030
+    e = 0.015134113397130
+    f = 0.999867560105837
+    return (
+        (b + (e * y) + (f * x)),
+        (a + (c * y) + (d * x)))
+
+HELSINKI_GROUPS = {
+    'skiing': {
+        2147483643: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483641: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483608: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483616: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483630: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483631: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483633: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483642: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483637: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483634: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483610: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483609: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483612: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483615: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483614: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483628: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        2147483629: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' },
+        214748362: { 'maintenance_subgroup_id': 'ita', 'maintenance_subgroup_name': 'Itä' } # TOODO: ID faulty
+    }
+}
+
+
 class Command(BaseCommand):
     help = "Import ski track units from GeoJSON derived from mSki"
     option_list = list(BaseCommand.option_list + (
@@ -64,9 +99,7 @@ class Command(BaseCommand):
                 defaults=defaults)
             unit.services.add(self.ski_service)
 
-    def import_vantaa_units(self, filename):
-        ds = DataSource(filename)
-        assert(len(ds) == 1)
+    def get_lowest_high_unit_id(self):
         uid = Unit.objects.aggregate(db.models.Max('id'))['id__max']
         while True:
             try:
@@ -74,8 +107,14 @@ class Command(BaseCommand):
                 uid -= 1
             except Unit.DoesNotExist as e:
                 break
+        return uid
+
+    def import_vantaa_units(self, filename):
+        ds = DataSource(filename)
+        assert(len(ds) == 1)
         lyr = ds[0]
         srs = lyr.srs
+        uid = self.get_lowest_high_unit_id()
         for feat in lyr:
             assert feat.geom_type == 'LineString'
             if type(feat.geom) == django.contrib.gis.gdal.geometries.MultiLineString:
@@ -94,7 +133,32 @@ class Command(BaseCommand):
             uid -= 1
 
     def import_espoo_units(self, filename):
-        pass
+        ds = DataSource(filename)
+        assert len(ds) == 1
+        uid = self.get_lowest_high_unit_id()
+        lyr = ds[0]
+        for feat in lyr:
+            print(feat.get('NIMI'))
+            if type(feat.geom) == django.contrib.gis.gdal.geometries.MultiLineString:
+                multilinestring = GEOSGeometry(feat.geom.wkt)
+            else:
+                multilinestring = MultiLineString(GEOSGeometry(feat.geom.wkt))
+            converted_multilinestring_coords = []
+            for line in multilinestring:
+                converted_multilinestring_coords.append(
+                    LineString(tuple((espoo_coordinates_to_gk25(point[0], point[1]) for point in line))))
+
+            converted_multilinestring = (
+                MultiLineString((converted_multilinestring_coords), srid=3879))
+            defaults = self.unit_defaults(
+                feat.get('NIMI'),
+                converted_multilinestring,
+                Point(converted_multilinestring[0][0], converted_multilinestring[0][1], srid=3879))
+            unit, created = Unit.objects.get_or_create(
+                pk=uid,
+                defaults=defaults)
+            unit.services.add(self.ski_service)
+            uid -= 1
 
     def handle(self, **options):
         self.options = options
