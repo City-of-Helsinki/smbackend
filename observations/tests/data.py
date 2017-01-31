89590822439c78c43f23b07a84fd171fa36bb204
Timo Tuominen <tituomin@gmail.com>
2017-01-31 18:18:50 +0200

    Fix test data.

--- observations/tests/data.py
+++ observations/tests/data.py
@@ -2,8 +2,7 @@ import datetime as d
 
 def observation_raw_data(observable_property_name, unit,
                            allowed_values=set()):
-
-    if observable_property_name == 'skiing_track_condition':
+    if observable_property_name == 'skiing_trail_condition':
         for val in allowed_values:
             yield dict(
                 unit=unit.pk,
