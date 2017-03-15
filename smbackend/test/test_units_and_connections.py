114a39d306d7bb6f4e9f804b27db28e0a5d4de15
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2017-03-15 15:49:04 +0200

    cleaned testing code, added instructions

--- smbackend/test/test_units_and_connections.py
+++ smbackend/test/test_units_and_connections.py
@@ -1,36 +1,34 @@
 import os
 
 import pytest
-import requests
 import requests_mock
 from django.core.management import call_command
 from django.utils.six import StringIO
 
 from services.management.commands.services_import import URL_BASE as TPR_URL
-from services.management.commands.services_import import Command as ServiceImportCommand
-# TPR_URL = "www.hel.fi/palvelukarttaws/rest/v4/"
 from services.models import Unit
 
 TPR_MOCK_DIR = os.path.join(os.path.dirname(__file__), 'tpr_mock_data')
 
-@pytest.fixture(scope="module")
-def smtp():
-    return smtplib.SMTP("smtp.gmail.com")
+# @pytest.fixture(scope="module")
+# def smtp():
+#     return smtplib.SMTP("smtp.gmail.com")
 
 
 @pytest.mark.django_db
 def test_unit_updating():
+    """ This test requires a fixture for munigeo. Generate one with:
+        $ ./manage.py dumpdata --format=json --indent=2 > smbackend/test/fixtures/munigeo.json
+        Also to moch the TPR API we need to include the response content. Generate the responses by running the
+        included script:
+        $ python generate_tpr_mock_data.py """
     with requests_mock.Mocker() as mock:
         call_command('loaddata', 'smbackend/test/fixtures/munigeo.json')
         mock_tpr(mock)
-        # response = requests.get('http://www.hel.fi/palvelukarttaws/rest/v3/connection/')
-        # assert 'paivahoito-ja-koulutus' in response.text
         assert Unit.objects.count() == 0
-        # cmd = ServiceImportCommand()
-        # cmd.verbosity = 0
-        # cmd.import_units()
         out = StringIO()
-        call_command('services_import', traceback=True, organizations=True, departments=True, services=True, units=True, stdout=out)
+        call_command('services_import', traceback=True, organizations=True, departments=True, services=True, units=True,
+                     stdout=out)
         assert Unit.objects.count() > 0
 
 
