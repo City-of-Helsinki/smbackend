e3079eb98b8ece22a90baa37df696cdb8fb87b0c
azf <i@azf.fi>
2017-03-30 22:43:33 +0300

    service import cleanup

--- services/models/service.py
+++ services/models/service.py
@@ -19,6 +19,7 @@ class Service(models.Model):
         return "%s (%s)" % (get_translated(self, 'name'), self.id)
 
 
+
 class ServiceManager(TreeManager):
     def get_queryset(self):
         return ServiceQuerySet(self.model, using=self._db)
