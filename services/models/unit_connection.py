ca7d897143272f16030f58db589270cfd2959fcb
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2017-04-12 13:58:04 +0300

    removed commented api structure

--- services/models/unit_connection.py
+++ services/models/unit_connection.py
@@ -20,30 +20,7 @@ class UnitConnection(models.Model):
     email = models.EmailField(max_length=100, null=True)
     phone = models.CharField(max_length=50, null=True)
     contact_person = models.CharField(max_length=80, null=True)
-
-    # section = models.CharField(max_length=20)
-    # phone_mobile = models.CharField(max_length=50, null=True)
     order = models.PositiveSmallIntegerField(default=0)
 
     class Meta:
         ordering = ['order']
-
-
-    # {'contact_person': 'Miia Kovalainen',
-    # 'email': 'miia.kovalainen@hel.fi',
-    # 'name_en': 'Head of day care centre',
-    # 'name_fi': 'Päiväkodinjohtaja',
-    # 'name_sv': 'Daghemsföreståndare',
-    # 'phone': '09 310 41571',
-    # 'section_type': 'PHONE_OR_EMAIL',
-    # 'unit_id': 1},
-
-    # {'name_en': 'Application for day care',
-    # 'name_fi': 'Täytä päivähoitohakemus',
-    # 'name_sv': 'Ansökan om barndagvård',
-    # 'section_type': 'LINK',
-    # 'unit_id': 1,
-    # 'www_en': 'http://www.hel.fi/www/helsinki/en/day-care-education/day-care/options/applying',
-    # 'www_fi': 'http://www.hel.fi/www/Helsinki/fi/paivahoito-ja-koulutus/paivahoito/paivakotihoito/hakeminen',
-    # 'www_sv': 'http://www.hel.fi/www/helsinki/sv/dagvard-och-utbildning/dagvord/ansokan/ansokan-dagvard'}
-
