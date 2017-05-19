eee967394aa6429df10788090ab35b2dec545dc9
Timo Tuominen <tituomin@gmail.com>
2017-05-19 16:20:43 +0300

    Extract generic tree model stuff into module

---
+++ services/models/hierarchy.py
@@ -0,0 +1,30 @@
+from django.db.models import QuerySet, Q
+from mptt.models import TreeManager
+
+
+class CustomTreeManager(TreeManager):
+    def get_queryset(self):
+        return TreeQuerySet(self.model, using=self._db)
+
+    def determine_max_level(self):
+        if hasattr(self, '_max_level'):
+            return self._max_level
+        qs = self.all().order_by('-level')
+        if qs.count():
+            self._max_level = qs[0].level
+        else:
+            # Harrison-Stetson method
+            self._max_level = 10
+        return self._max_level
+
+
+class TreeQuerySet(QuerySet):
+    def by_ancestor(self, ancestor):
+        manager = self.model.objects
+        max_level = manager.determine_max_level()
+        qs = Q()
+        # Construct an OR'd queryset for each level of parenthood.
+        for i in range(max_level):
+            key = '__'.join(['parent'] * (i + 1))
+            qs |= Q(**{key: ancestor})
+        return self.filter(qs)
--- services/models/ontology_tree_node.py
+++ services/models/ontology_tree_node.py
@@ -1,25 +1,9 @@
 from django.db import models
-from django.db.models import QuerySet, Q
-from mptt.models import MPTTModel, TreeForeignKey, TreeManager
+from mptt.models import MPTTModel, TreeForeignKey
 from services.utils import get_translated
 from .keyword import Keyword
 from .unit import Unit
-
-
-class OntologyTreeManager(TreeManager):
-    def get_queryset(self):
-        return OntologyTreeQuerySet(self.model, using=self._db)
-
-    def determine_max_level(self):
-        if hasattr(self, '_max_level'):
-            return self._max_level
-        qs = self.all().order_by('-level')
-        if qs.count():
-            self._max_level = qs[0].level
-        else:
-            # Harrison-Stetson method
-            self._max_level = 10
-        return self._max_level
+from .hierarchy import CustomTreeManager
 
 
 class OntologyTreeNode(MPTTModel):
@@ -33,7 +17,7 @@ class OntologyTreeNode(MPTTModel):
 
     last_modified_time = models.DateTimeField(db_index=True, help_text='Time of last modification')
 
-    objects = OntologyTreeManager()
+    objects = CustomTreeManager()
 
     def __str__(self):
         return "%s (%s)" % (get_translated(self, 'name'), self.id)
@@ -43,15 +27,3 @@ class OntologyTreeNode(MPTTModel):
         srv_list.add(self.id)
         count = Unit.objects.filter(service_tree_nodes__in=list(srv_list)).distinct().count()
         return count
-
-
-class OntologyTreeQuerySet(QuerySet):
-    def by_ancestor(self, ancestor):
-        manager = self.model.objects
-        max_level = manager.determine_max_level()
-        qs = Q()
-        # Construct an OR'd queryset for each level of parenthood.
-        for i in range(max_level):
-            key = '__'.join(['parent'] * (i + 1))
-            qs |= Q(**{key: ancestor})
-        return self.filter(qs)
