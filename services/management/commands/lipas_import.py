32c04689d2a4d8e09d42c46a09754adaa8ec8e44
Elias Benkhodja <elias.benkhodja@gmail.com>
2017-06-27 18:06:34 +0300

    Improve the Lipas importer
    - Improve logging
    - Simplify GeometryCollections
    - Restructure a little

--- services/management/commands/lipas_import.py
+++ services/management/commands/lipas_import.py
@@ -12,7 +12,6 @@ URLS = {
 }
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 
 class Command(BaseCommand):
@@ -27,18 +26,14 @@ class Command(BaseCommand):
         for unit_identifier in unit_identifiers:
             units_by_lipas_id[int(unit_identifier.value)] = unit_identifier.unit
 
+        logger.info('Retrieved {} objects.'.format(len(unit_identifiers)))
+
         # Get path and area data from the Lipas WFS
-        logger.info('Done.')
         logger.info('Retrieving geodata from Lipas...')
-        ds_paths = DataSource(URLS['paths'])
-        ds_areas = DataSource(URLS['areas'])
-        logger.info('Done.')
-
-        # Ensure the data looks somewhat correct
-        if len(ds_paths) != 1 or len(ds_areas) != 1:
-            raise ValueError('The LIPAS API returned unexpected data!')
-
-        layers = [ds_paths[0], ds_areas[0]]
+        layers = {}
+        layers['paths'] = DataSource(URLS['paths'])[0]
+        layers['areas'] = DataSource(URLS['areas'])[0]
+        logger.info('Retrieved {} path and {} area features.'.format(len(layers['paths']), len(layers['areas'])))
 
         # The Lipas database stores paths and areas as different features
         # which have a common id. We want to store the paths as one
@@ -50,33 +45,37 @@ class Command(BaseCommand):
 
         # Iterate through Lipas layers and features
         logger.info('Processing Lipas geodata...')
-        for layer in layers:
+        for _, layer in layers.items():
             for feature in layer:
                 logger.debug(feature.fid)
 
                 # Check if the feature's id is in the dict we built earlier
-                if feature['id'].value in units_by_lipas_id:
-                    logger.debug('found: ' + units_by_lipas_id[feature['id'].value].name_fi)
+                lipas_id = feature['id'].value
+                unit = units_by_lipas_id.get(lipas_id)
+                if not unit:
+                    logging.debug('id not found: {}'.format(lipas_id))
+                    continue
 
-                    # TODO Check that they surely are the same unit
-                    logger.debug('confirm: ' + feature['nimi_fi'].value)
+                logger.debug('found id: '.format(lipas_id))
 
-                    # Initialize an empty GeometryCollection if we haven't encountered
-                    # this id before. Elsewise just append to the collection.
-                    if feature['id'].value in geometries:
-                        geometries[feature['id'].value].append(feature.geom.geos)
-                    else:
-                        geometries[feature['id'].value] = GeometryCollection(feature.geom.geos)
+                if feature['nimi_fi'].value != unit.name_fi:
+                    logger.warning('id {} has non-matching name fields.\n'
+                                   'Lipas: "{}"\n'
+                                   'db: "{}"'.format(lipas_id, feature['nimi_fi'].value, unit.name_fi))
 
+                # Initialize an empty GeometryCollection if we haven't encountered
+                # this id before. Elsewise just append to the collection.
+                if lipas_id in geometries:
+                    geometries[lipas_id].append(feature.geom.geos)
                 else:
-                    logging.debug('id not found: ' + str(feature['id'].value))
+                    geometries[lipas_id] = GeometryCollection(feature.geom.geos)
 
-        logger.info('Done. Found {} matches'.format(len(geometries)))
+        logger.info('Found {} matches.'.format(len(geometries)))
 
         # Add all geometries we found to the db
         logger.info('Updating geometries in the database...')
         for lipas_id, geometry in geometries.items():
-            units_by_lipas_id[lipas_id].geometry = geometry
-            units_by_lipas_id[lipas_id].save()
-
-        logger.info('Done.')
+            unit = units_by_lipas_id[lipas_id]
+            # Simplify our GeometryCollections as much as possible
+            unit.geometry = geometry.unary_union
+            unit.save()
