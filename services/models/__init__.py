5d4f091cdc1fae79ebba8a9a133fb3186bac7c73
Timo Tuominen <tituomin@gmail.com>
2018-05-02 19:07:28 +0300

    Remove accessibility sentences

--- services/models/__init__.py
+++ services/models/__init__.py
@@ -6,7 +6,6 @@ from .keyword import Keyword
 from .unit_connection import UnitConnection
 from .unit_accessibility_property import UnitAccessibilityProperty
 from .accessibility_variable import AccessibilityVariable
-from .accessibility_sentence import AccessibilitySentence
 from .unit_identifier import UnitIdentifier
 from .unit_alias import UnitAlias
 from .service_mapping import ServiceMapping
