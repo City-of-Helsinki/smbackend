859df1013254dffceecdcbceb92f585bc4107d64
Timo Tuominen <tituomin@gmail.com>
2018-08-14 17:25:00 +0300

    Remove unnecessary TLS patch for Open311

--- services/views.py
+++ services/views.py
@@ -1,9 +1,8 @@
-import json
 import requests
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-from .patch_ssl import get_session
+
 
 @csrf_exempt
 def post_service_request(request):
@@ -19,7 +18,7 @@ def post_service_request(request):
         api_key = settings.OPEN311['API_KEY']
     outgoing['api_key'] = api_key
     url = settings.OPEN311['URL_BASE']
-    session = get_session()
+    session = requests.Session()
     r = session.post(url, data=outgoing)
     if r.status_code != 200:
         return HttpResponseBadRequest()
