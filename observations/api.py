724be11c40e67921909de28138d494c14c426ad5
MiiliH <miili.halkka@aalto.fi>
2018-11-09 12:28:39 +0200

    fixed api.py

--- observations/api.py
+++ observations/api.py
@@ -1,26 +1,27 @@
-from rest_framework import serializers, viewsets
-from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed
 from django.utils.translation import ugettext_lazy as _
-
-from services.api import OntologyWordSerializer as ServiceSerializer, UnitSerializer
+from services.api import (
+    OntologyWordSerializer as ServiceSerializer, UnitSerializer)
 from . import models
-from .serializers import *
-
-from django.contrib.contenttypes.models import ContentType
-
-
+from .serializers import ObservationSerializer, ObservablePropertySerializer
 from services.api import (
     JSONAPIViewSetMixin, OntologyWordViewSet as ServiceViewSet, UnitViewSet)
+
 
 class ObservationViewSet(JSONAPIViewSetMixin, viewsets.ModelViewSet):
     queryset = models.Observation.objects.all()
     serializer_class = ObservationSerializer
+
     def create(self, request, *args, **kwargs):
-        if (request.auth == None):
+        if (request.auth is None):
             raise AuthenticationFailed(_('Authentication required.'))
+
         return super(ObservationViewSet, self).create(request, *args, **kwargs)
+
     def get_serializer_context(self):
         return {'user': self.request.user, 'auth': self.request.auth}
+
 
 class ObservableSerializerMixin:
     def to_representation(self, obj):
@@ -31,24 +32,35 @@ class ObservableSerializerMixin:
         if 'observations' in self.context.get('include', []):
             observations = self.get_observations(obj)
             if observations:
-                data['observations'] = ObservationSerializer(observations, many=True).data
+                data['observations'] = ObservationSerializer(
+                    observations, many=True).data
+
         return data
 
-class ObservableServiceSerializer(ObservableSerializerMixin, ServiceSerializer):
+
+class ObservableServiceSerializer(
+        ObservableSerializerMixin, ServiceSerializer):
     def get_observable_properties(self, service):
         return service.observable_properties.all()
+
     def get_observations(self, service):
         return None
 
+
 class ObservableUnitSerializer(ObservableSerializerMixin, UnitSerializer):
     def get_observable_properties(self, unit):
-        return models.ObservableProperty.objects.filter(services__in=unit.services.all())
+        return models.ObservableProperty.objects.filter(
+            services__in=unit.services.all())
+
     def get_observations(self, unit):
         return unit.observation_set
 
+
 UnitViewSet.serializer_class = ObservableUnitSerializer
+
+
 ServiceViewSet.serializer_class = ObservableServiceSerializer
 
 views = [
-    {'class': ObservationViewSet, 'name': 'observation' }
+    {'class': ObservationViewSet, 'name': 'observation'}
 ]
