4f2e80b4cad4d13806a9ea240dc9f7a382b7a51d
Hannes Alarotu <hannes.alarotu@codento.com>
2018-11-22 11:31:55 +0200

    allow other cities for user

--- services/management/commands/user_import.py
+++ services/management/commands/user_import.py
@@ -7,7 +7,7 @@ from observations.models import UserOrganization
 
 
 class Command(BaseCommand):
-    help = "Import users, manage.py users_import username password city"
+    help = "Import users, manage.py user_import username password city"
 
     def add_arguments(self, parser):
         parser.add_argument('username')
@@ -23,7 +23,6 @@ class Command(BaseCommand):
 
         username=options['username']
         password=options['password']
-        city=None
 
         if options['city'] == 'espoo':
             city = '520a4492-cb78-498b-9c82-86504de88dce'
@@ -33,11 +32,12 @@ class Command(BaseCommand):
             city = '6f0458d4-42a3-434a-b9be-20c19fcfa5c3'
         elif options['city'] == 'vantaa':
             city = '6d78f89c-9fd7-41d9-84e0-4b78c0fa25ce'
+        else:
+            city = options['city']
 
-        #User.objects.filter(username='pulkka').delete()
-        if city is not None:
-            department = Department.objects.get(uuid=city)
+        department = Department.objects.get(uuid=city)
 
+        if department is not None:
             user = User.objects.filter(username=username)
             if len(user) == 0:
                 user = User.objects.create_user(username, password=password)
@@ -54,4 +54,4 @@ class Command(BaseCommand):
                 self.stdout.write('User {} already exists'.format(username))
 
         else:
-            self.stdout.write('City argument is not one of the following: espoo, helsinki, kauniainen, vantaa')
+            self.stdout.write('City {} not found'.format(city))
