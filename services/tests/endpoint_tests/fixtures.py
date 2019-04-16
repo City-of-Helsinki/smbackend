bea93961fb3e9ef5f9cc07d64f20106e340219b5
Evgenia Lyjina <jenny.lyjina@gmail.com>
2019-04-16 13:57:13 +0300

    Adds tests for unit endpoint filters with several parameters: id, municipality, city_as_department.

--- services/tests/endpoint_tests/fixtures.py
+++ services/tests/endpoint_tests/fixtures.py
@@ -23,7 +23,7 @@ def municipalities():
                                                                   ocd_id='ocd-division/country:fi/kunta:muni_'+n)
         if created:
             Municipality.objects.get_or_create(id='muni_'+n, name_fi='muni_'+n, division=a)
-    print('MUNIS', Municipality.objects.all().order_by('pk'))
+    # print('MUNIS', Municipality.objects.all().order_by('pk'))
     return Municipality.objects.all().order_by('pk')
 
 
@@ -35,7 +35,7 @@ def departments(municipalities):
                                      municipality=muni_0)
     Department.objects.get_or_create(uuid='92f9182e-0942-4d82-8b6a-09499fe9c46a', name='dep_1',
                                      municipality=muni_1)
-    print('DEPS', Department.objects.all().order_by('pk'))
+    # print('DEPS', Department.objects.all().order_by('pk'))
     return Department.objects.all().order_by('pk')
 
 
@@ -45,6 +45,6 @@ def units(municipalities, departments):
     for i in range(len(departments)):
         Unit.objects.get_or_create(pk=i, name_fi='unit_'+str(i), municipality=municipalities[i],
                                    root_department=departments[i], last_modified_time=TODAY)
-    print('UNITS', Unit.objects.values())
+    # print('UNITS', Unit.objects.values())
     return Unit.objects.all().order_by('pk')
 
--- services/tests/endpoint_tests/test_unit_endpoint.py
+++ services/tests/endpoint_tests/test_unit_endpoint.py
@@ -7,7 +7,6 @@ from services.tests.utils import get
 
 def get_unit_list(api_client, data=None, query_string=None):
     url = reverse('unit-list')
-    print(url)
     if query_string:
         url = '%s?%s' % (url, query_string)
         print(url)
@@ -16,10 +15,24 @@ def get_unit_list(api_client, data=None, query_string=None):
 
 
 @pytest.mark.django_db
-def test_unit_id_filter(units, api_client):
+def test_unit_id_filter_one_value(units, api_client):
     res = get_unit_list(api_client, query_string='id=0')
     assert len(res.data['results']) == 1
     assert res.data['results'][0]['id'] == 0
+
+
+@pytest.mark.django_db
+def test_unit_id_filter_several_values(units, api_client):
+    res = get_unit_list(api_client, query_string='id=0,1')
+    assert len(res.data['results']) == 2
+    ids = [0, 1]
+    for i in range(2):
+        try:
+            assert res.data['results'][i]['id'] == ids[0]
+            ids.pop(0)
+        except:
+            assert res.data['results'][i]['id'] == ids[1]
+            ids.pop(1)
 
 
 @pytest.mark.django_db
@@ -30,8 +43,36 @@ def test_municipality_filter(units, api_client):
 
 
 @pytest.mark.django_db
+def test_municipality_filter_several_values(units, api_client):
+    munis = ['muni_0', 'muni_1']
+    res = get_unit_list(api_client, query_string='municipality={},{}'.format(munis[0], munis[1]))
+    assert len(res.data['results']) == 2
+    for i in range(2):
+        try:
+            assert res.data['results'][i]['municipality'] == munis[0]
+            munis.pop(0)
+        except:
+            assert res.data['results'][i]['municipality'] == munis[1]
+            munis.pop(1)
+
+
+@pytest.mark.django_db
 def test_city_as_department_filter(units, api_client):
     res = get_unit_list(api_client, query_string='city_as_department=da792f32-6da7-4804-8059-16491b1ec0fa')
     assert len(res.data['results']) == 1
     assert res.data['results'][0]['root_department'] == UUID('da792f32-6da7-4804-8059-16491b1ec0fa')
     assert res.data['results'][0]['municipality'] == 'muni_0'
+
+
+@pytest.mark.django_db
+def test_city_as_department_filter_several_values(units, api_client):
+    deps = ['da792f32-6da7-4804-8059-16491b1ec0fa', '92f9182e-0942-4d82-8b6a-09499fe9c46a']
+    res = get_unit_list(api_client, query_string='city_as_department={0},{1}'.format(deps[0], deps[1]))
+    assert len(res.data['results']) == 2
+    for i in range(2):
+        try:
+            assert res.data['results'][i]['root_department'] == UUID(deps[0])
+            deps.pop(0)
+        except:
+            assert res.data['results'][i]['root_department'] == UUID(deps[1])
+            deps.pop(1)
