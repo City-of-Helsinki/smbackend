694a484d2a7ebd4380c26e4d6b88442ddf991ac9
Evgenia Lyjina <jenny.lyjina@gmail.com>
2019-04-25 19:24:59 +0300

    WIP. Adds tests for service endpoint filters: only, include, page and page_size.

---
+++ services/tests/endpoint_tests/test_service_endpoint.py
@@ -0,0 +1,59 @@
+import pytest  # noqa: F401;
+
+from rest_framework.reverse import reverse
+
+from services.tests.endpoint_tests.fixtures import *  # noqa: F403, F401;
+from services.tests.utils import get
+
+
+def get_service_list(api_client, data=None, query_string=None):
+    url = reverse('service-list')
+    if query_string:
+        url = '%s?%s' % (url, query_string)
+        print(url)
+    res = get(api_client, url, data=data).data['results']
+    res.sort(key=lambda r: r['id'])
+    return res
+
+
+@pytest.mark.django_db
+def test_page_filter(services, api_client):
+    res = get_service_list(api_client, query_string='page=1')
+    assert len(res) == 4
+    assert res[0]['id'] == 0
+    assert res[3]['id'] == 3
+
+    res = get_service_list(api_client, query_string='page=1&page_size=2')
+    assert len(res) == 2
+    assert res[0]['id'] == 2
+    assert res[1]['id'] == 3
+
+    res = get_service_list(api_client, query_string='page=2&page_size=2')
+    assert len(res) == 2
+    assert res[0]['id'] == 0
+    assert res[1]['id'] == 1
+
+
+@pytest.mark.django_db
+def test_only_filter(services, api_client):
+    res = get_service_list(api_client, query_string='only=id')
+    assert len(res) == 4
+    assert res[0]['id'] == 0
+    assert res[0].get('name') is None
+
+
+@pytest.mark.django_db
+def test_only_filter_several_values(services, api_client):
+    res = get_service_list(api_client, query_string='only=id,name')
+    assert len(res) == 4
+    assert res[0]['id'] == 0
+    assert res[0]['name'] == {'fi': 'service_0'}
+
+
+@pytest.mark.skip(reason="test not working, temporary disabled")
+@pytest.mark.django_db
+def test_include_filter(services, api_client):
+    res = get_service_list(api_client, query_string='include=root_service_node')
+    print(res)
+    assert len(res) == 4
+    assert res[0]['root_service_node']['name'] == 'service_node_0'
