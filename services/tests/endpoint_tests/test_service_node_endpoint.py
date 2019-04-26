5982e48fbbf441a24504475264192c18e093655a
Evgenia Lyjina <jenny.lyjina@gmail.com>
2019-04-26 13:24:38 +0300

    Adds tests for service_nodes endpoint filters: only, include, page and page_size and id.

---
+++ services/tests/endpoint_tests/test_service_node_endpoint.py
@@ -0,0 +1,74 @@
+import pytest  # noqa: F401;
+
+from rest_framework.reverse import reverse
+
+from services.tests.endpoint_tests.fixtures import *  # noqa: F403, F401;
+from services.tests.utils import get
+
+
+def get_service_node_list(api_client, data=None, query_string=None):
+    url = reverse('servicenode-list')
+    if query_string:
+        url = '%s?%s' % (url, query_string)
+        print(url)
+    res = get(api_client, url, data=data).data['results']
+    res.sort(key=lambda r: r['id'])
+    return res
+
+
+@pytest.mark.django_db
+def test_page_filter(service_nodes, api_client):
+    res = get_service_node_list(api_client, query_string='page=1')
+    assert len(res) == 4
+    assert res[0]['id'] == 0
+    assert res[3]['id'] == 3
+
+    res = get_service_node_list(api_client, query_string='page=1&page_size=2')
+    assert len(res) == 2
+    assert res[0]['id'] == 2
+    assert res[1]['id'] == 3
+
+    res = get_service_node_list(api_client, query_string='page=2&page_size=2')
+    assert len(res) == 2
+    assert res[0]['id'] == 0
+    assert res[1]['id'] == 1
+
+
+@pytest.mark.django_db
+def test_only_filter(service_nodes, api_client):
+    res = get_service_node_list(api_client, query_string='only=id')
+    assert len(res) == 4
+    assert res[0]['id'] == 0
+    assert res[0].get('name') is None
+
+
+@pytest.mark.django_db
+def test_only_filter_several_values(service_nodes, api_client):
+    res = get_service_node_list(api_client, query_string='only=id,name')
+    assert len(res) == 4
+    assert res[0]['id'] == 0
+    assert res[0]['name'] == {'fi': 'servicenode_0'}
+
+
+@pytest.mark.django_db
+def test_include_filter(service_nodes, services, api_client):
+    res = get_service_node_list(api_client, query_string='include=related_services')
+    assert len(res) == 4
+    assert len(res[0]['related_services']) == 1
+    assert res[0]['related_services'][0]['name'] == {'fi': 'service_0'}
+
+
+@pytest.mark.django_db
+def test_service_node_id_filter(service_nodes, api_client):
+    res = get_service_node_list(api_client, query_string='id=0')
+    assert len(res) == 1
+    assert res[0]['id'] == 0
+
+
+@pytest.mark.django_db
+def test_id_filter_several_values(service_nodes, api_client):
+    ids = [0, 1]
+    res = get_service_node_list(api_client, query_string='id={0},{1}'.format(ids[0], ids[1]))
+    assert len(res) == 2
+    for i in range(len(res)):
+        assert res[i]['id'] == ids[i]
