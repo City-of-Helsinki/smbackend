aa2a6ee0ff6daf7b76a891f96dd6c276429ac0bf
Timo Tuominen <tituomin@gmail.com>
2019-10-04 15:06:30 +0300

    Publish suggestions under API endpoint

--- services/views.py
+++ services/views.py
@@ -1,13 +1,23 @@
 import requests
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+
+from .search_suggestions import get_suggestions
+
+
+@require_http_methods(['GET', 'POST'])
+def suggest(request):
+    query = request.GET['q']
+    if not query:
+        return HttpResponseBadRequest()
+    return JsonResponse(get_suggestions(query), content_type='application/json')
 
 
 @csrf_exempt
+@require_http_methods(['POST'])
 def post_service_request(request):
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'])
     payload = request.POST.copy()
     outgoing = payload.dict()
     if outgoing.get('internal_feedback', False):
@@ -23,4 +33,4 @@ def post_service_request(request):
     if r.status_code != 200:
         return HttpResponseBadRequest()
 
-    return HttpResponse(r.content, content_type="application/json")
+    return HttpResponse(r.content, content_type='application/json')
