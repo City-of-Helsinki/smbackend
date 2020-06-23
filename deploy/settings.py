1718b2eb4a66137deac5536f557ee05ef4a4cf3c
Mika Hietanen <m.hietanen84@gmail.com>
2020-06-23 08:05:26 +0300

    Fix flake8 warnings

--- deploy/settings.py
+++ deploy/settings.py
@@ -1,17 +1,15 @@
-import environ
-
-from smbackend.settings import *  # noqa
+import smbackend.settings
 
 # Get whitenoise for serving static files
 try:
-    place = MIDDLEWARE.index('django.middleware.security.SecurityMiddleware')
+    place = smbackend.settings.MIDDLEWARE.index('django.middleware.security.SecurityMiddleware')
 except ValueError:
     place = 0
 
-MIDDLEWARE.insert(place, 'whitenoise.middleware.WhiteNoiseMiddleware')
+smbackend.settings.MIDDLEWARE.insert(place, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 
-deploy_env = environ.Env(
+deploy_env = smbackend.settings.environ.Env(
     USE_X_FORWARDED_HOST=(bool, False),
     SECURE_PROXY=(bool, False),
     MEDIA_ROOT=(str, "/usr/src/app/www")
