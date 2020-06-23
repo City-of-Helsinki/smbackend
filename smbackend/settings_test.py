8009ec2c40fc4423626c354e875e32faa4c2bed3
Mika Hietanen <m.hietanen84@gmail.com>
2020-06-23 08:48:06 +0300

    Add servicemap-ui to docker-compose configuration
    Add servicemap-ui to docker-compose configuration.
    Simplify gitignore-rules.
    Update README.

--- smbackend/settings_test.py
+++ smbackend/settings_test.py
@@ -2,13 +2,3 @@ from .settings import *
 
 # Not needed in tests, slows processing
 DISABLE_HAYSTACK_SIGNAL_PROCESSOR = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'HOST': '127.0.0.1',
-        'NAME': 'smbackend',
-        'USER': 'smbackend',
-        'PASSWORD': 'smbackend',
-    }
-}
