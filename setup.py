f7057d6abe751a15249c7d111661d3a150e97332
Mika Hietanen <m.hietanen84@gmail.com>
2020-07-08 12:52:48 +0300

    Update project settings
    Add isort and black with settings to the project.
    Change to use setup.py and setup.cfg for common settings.
    Update project settings for flake8 and pytest.
    Update requirements.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="smbackend",
+    version="200615",
+    license="AGPLv3",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        l
+        for l in open("requirements.txt", "rt").readlines()
+        if l and not l.startswith("#")
+    ],
+    zip_safe=False,
+)
