94e03314bdc1bafbd87ccce624ec9b4bcb9ce6a6
Arjeta Peshtani <arjeta.peshtani@anders.fi>
2020-08-19 15:34:14 +0300

    Apply flake8, isort and black rules to Turku importers

--- smbackend_turku/importers/services.py
+++ smbackend_turku/importers/services.py
@@ -1,18 +1,22 @@
-from datetime import datetime
-
 import pytz
+from datetime import datetime
 from munigeo.importer.sync import ModelSyncher
 
 from services.management.commands.services_import.keyword import KeywordHandler
-from services.management.commands.services_import.services import update_service_root_service_nodes
+from services.management.commands.services_import.services import (
+    update_service_root_service_nodes,
+)
+from services.models import Service, ServiceNode
+from smbackend_turku.importers.utils import (
+    convert_code_to_int,
+    get_turku_resource,
+    set_syncher_object_field,
+    set_syncher_tku_translated_field,
+)
 
-from services.models import ServiceNode, Service
-from smbackend_turku.importers.utils import get_turku_resource, set_syncher_tku_translated_field, \
-    set_syncher_object_field, convert_code_to_int
+UTC_TIMEZONE = pytz.timezone("UTC")
 
-UTC_TIMEZONE = pytz.timezone('UTC')
-
-SERVICE_AS_SERVICE_NODE_PREFIX = 'service_'
+SERVICE_AS_SERVICE_NODE_PREFIX = "service_"
 
 BLACKLISTED_SERVICE_NODES = [
     "2_1",
@@ -35,17 +39,17 @@ class ServiceImporter:
         self._import_service_nodes(keyword_handler)
 
     def _import_service_nodes(self, keyword_handler):
-        service_classes = get_turku_resource('palveluluokat')
+        service_classes = get_turku_resource("palveluluokat")
 
         tree = self._build_servicetree(service_classes)
         for parent_node in tree:
-            if parent_node['koodi'] in BLACKLISTED_SERVICE_NODES:
+            if parent_node["koodi"] in BLACKLISTED_SERVICE_NODES:
                 continue
             self._handle_service_node(parent_node, keyword_handler)
         self.nodesyncher.finish()
 
     def _import_services(self, keyword_handler):
-        services = get_turku_resource('palvelut')
+        services = get_turku_resource("palvelut")
 
         for service in services:
             self._handle_service(service, keyword_handler)
@@ -59,7 +63,7 @@ class ServiceImporter:
                 self.importer.services_changed = True
 
     def _build_servicetree(self, service_classes):
-        tree = [s_cls for s_cls in service_classes if 'ylatason_koodi' not in s_cls]
+        tree = [s_cls for s_cls in service_classes if "ylatason_koodi" not in s_cls]
         for parent in tree:
             self._add_service_tree_children(parent, service_classes)
 
@@ -73,25 +77,25 @@ class ServiceImporter:
             == convert_code_to_int(parent_classes["koodi"])
         ]
 
-        for child_ot in parent_classes['children']:
+        for child_ot in parent_classes["children"]:
             self._add_service_tree_children(child_ot, service_classes)
 
     def _handle_service_node(self, node, keyword_handler):
-        node_id = convert_code_to_int(node['koodi'])
+        node_id = convert_code_to_int(node["koodi"])
         obj = self.nodesyncher.get(node_id)
         if not obj:
             obj = ServiceNode(id=node_id)
             obj._changed = True
 
-        if 'nimi_kieliversiot' in node:
-            set_syncher_tku_translated_field(obj, 'name', node.get('nimi_kieliversiot'))
+        if "nimi_kieliversiot" in node:
+            set_syncher_tku_translated_field(obj, "name", node.get("nimi_kieliversiot"))
         else:
-            name = node.get('nimi')
-            set_syncher_object_field(obj, 'name', name)
-            set_syncher_object_field(obj, 'name_fi', name)
+            name = node.get("nimi")
+            set_syncher_object_field(obj, "name", name)
+            set_syncher_object_field(obj, "name_fi", name)
 
-        if 'ylatason_koodi' in node:
-            parent_id = convert_code_to_int(node['ylatason_koodi'])
+        if "ylatason_koodi" in node:
+            parent_id = convert_code_to_int(node["ylatason_koodi"])
             parent = self.nodesyncher.get(parent_id)
             assert parent
         else:
@@ -102,22 +106,24 @@ class ServiceImporter:
 
         self._save_object(obj)
 
-        if not node['koodi'].startswith(SERVICE_AS_SERVICE_NODE_PREFIX):
+        if not node["koodi"].startswith(SERVICE_AS_SERVICE_NODE_PREFIX):
             self._handle_related_services(obj, node)
         else:
-            set_syncher_object_field(obj, 'service_reference', convert_code_to_int(node['koodi']))
+            set_syncher_object_field(
+                obj, "service_reference", convert_code_to_int(node["koodi"])
+            )
 
         self.nodesyncher.mark(obj)
 
-        for child_node in node['children']:
+        for child_node in node["children"]:
             self._handle_service_node(child_node, keyword_handler)
 
     def _handle_related_services(self, obj, node):
-        old_service_ids = set(obj.related_services.values_list('id', flat=True))
+        old_service_ids = set(obj.related_services.values_list("id", flat=True))
         obj.related_services.clear()
 
-        for service_data in node.get('palvelut', []):
-            service_id = int(service_data.get('koodi'))
+        for service_data in node.get("palvelut", []):
+            service_id = int(service_data.get("koodi"))
 
             try:
                 service = Service.objects.get(id=service_id)
@@ -128,23 +134,21 @@ class ServiceImporter:
 
             obj.related_services.add(service)
 
-        new_service_ids = set(obj.related_services.values_list('id', flat=True))
+        new_service_ids = set(obj.related_services.values_list("id", flat=True))
 
         if old_service_ids != new_service_ids:
             obj._changed = True
 
     def _handle_service(self, service, keyword_handler):
-        koodi = int(service['koodi'])  # Cast to int as koodi should always be a stringified integer
+        koodi = int(
+            service["koodi"]
+        )  # Cast to int as koodi should always be a stringified integer
         obj = self.servicesyncher.get(koodi)
         if not obj:
-            obj = Service(
-                id=koodi,
-                clarification_enabled=False,
-                period_enabled=False
-            )
+            obj = Service(id=koodi, clarification_enabled=False, period_enabled=False)
             obj._changed = True
 
-        set_syncher_tku_translated_field(obj, 'name', service.get('nimi_kieliversiot'))
+        set_syncher_tku_translated_field(obj, "name", service.get("nimi_kieliversiot"))
 
         obj._changed = keyword_handler.sync_searchwords(obj, service, obj._changed)
 
--- smbackend_turku/importers/utils.py
+++ smbackend_turku/importers/utils.py
@@ -2,24 +2,23 @@ import datetime
 import hashlib
 import os
 import re
-
 import requests
 from django.conf import settings
 
 # TODO: Change to production endpoint when available
-TURKU_BASE_URL = 'https://digiaurajoki.turku.fi:9443/kuntapalvelut/api/v1/'
-ACCESSIBILITY_BASE_URL = 'https://asiointi.hel.fi/kapaesteettomyys/api/v1/'
+TURKU_BASE_URL = "https://digiaurajoki.turku.fi:9443/kuntapalvelut/api/v1/"
+ACCESSIBILITY_BASE_URL = "https://asiointi.hel.fi/kapaesteettomyys/api/v1/"
 
 
 def clean_text(text, default=None):
     if not isinstance(text, str):
         return text
     # remove consecutive whitespaces
-    text = re.sub(r'\s\s+', ' ', text, re.U)
+    text = re.sub(r"\s\s+", " ", text, re.U)
     # remove nil bytes
-    text = text.replace('\u0000', ' ')
+    text = text.replace("\u0000", " ")
     text = text.replace("\r", "\n")
-    text = text.replace('\\r', "\n")
+    text = text.replace("\\r", "\n")
     text = text.strip()
     if len(text) == 0:
         return default
@@ -29,22 +28,22 @@ def clean_text(text, default=None):
 def get_resource(url, headers=None):
     print("CALLING URL >>> ", url)
     resp = requests.get(url, headers=headers)
-    assert resp.status_code == 200, 'status code {}'.format(resp.status_code)
+    assert resp.status_code == 200, "status code {}".format(resp.status_code)
     return resp.json()
 
 
-def get_turku_api_headers(content=''):
-    application = 'Palvelukartta'
-    key = getattr(settings, 'TURKU_API_KEY', '')
+def get_turku_api_headers(content=""):
+    application = "Palvelukartta"
+    key = getattr(settings, "TURKU_API_KEY", "")
     now = datetime.datetime.utcnow()
-    timestamp = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+    timestamp = now.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    data = (application + timestamp + content + key).encode('utf-8')
+    data = (application + timestamp + content + key).encode("utf-8")
     auth = hashlib.sha256(data)
     return {
-        'Authorization': auth.hexdigest(),
-        'X-TURKU-SP': application,
-        'X-TURKU-TS': timestamp
+        "Authorization": auth.hexdigest(),
+        "X-TURKU-SP": application,
+        "X-TURKU-TS": timestamp,
     }
 
 
@@ -54,22 +53,28 @@ def get_ar_resource(resource_name):
 
 
 def get_ar_servicepoint_resource(resource_name=None):
-    template_vars = [ACCESSIBILITY_BASE_URL, getattr(settings, 'ACCESSIBILITY_SYSTEM_ID', '')]
+    template_vars = [
+        ACCESSIBILITY_BASE_URL,
+        getattr(settings, "ACCESSIBILITY_SYSTEM_ID", ""),
+    ]
     url_template = "{}servicepoints/{}"
     if resource_name:
         template_vars.append(resource_name)
-        url_template += '/{}'
+        url_template += "/{}"
 
     url = url_template.format(*template_vars)
     return get_resource(url)
 
 
 def get_ar_servicepoint_accessibility_resource(resource_name=None):
-    template_vars = [ACCESSIBILITY_BASE_URL, getattr(settings, 'ACCESSIBILITY_SYSTEM_ID', '')]
+    template_vars = [
+        ACCESSIBILITY_BASE_URL,
+        getattr(settings, "ACCESSIBILITY_SYSTEM_ID", ""),
+    ]
     url_template = "{}accessibility/servicepoints/{}"
     if resource_name:
         template_vars.append(resource_name)
-        url_template += '/{}'
+        url_template += "/{}"
 
     url = url_template.format(*template_vars)
     return get_resource(url)
@@ -81,7 +86,9 @@ def get_turku_resource(resource_name):
     return get_resource(url, headers)
 
 
-def set_tku_translated_field(obj, obj_field_name, entry_data, max_length=None, clean=True):
+def set_tku_translated_field(
+    obj, obj_field_name, entry_data, max_length=None, clean=True
+):
     if not entry_data:
         return False
 
@@ -96,7 +103,7 @@ def set_tku_translated_field(obj, obj_field_name, entry_data, max_length=None, c
         if max_length and value and len(value) > max_length:
             value = None
 
-        obj_key = '{}_{}'.format(obj_field_name, language)
+        obj_key = "{}_{}".format(obj_field_name, language)
         obj_value = getattr(obj, obj_key)
 
         if obj_value == value:
@@ -126,44 +133,48 @@ def set_syncher_object_field(obj, obj_field_name, value):
     obj._changed |= set_field(obj, obj_field_name, value)
 
 
-def set_syncher_tku_translated_field(obj, obj_field_name, value, max_length=None, clean=True):
-    obj._changed |= set_tku_translated_field(obj, obj_field_name, value, max_length, clean)
+def set_syncher_tku_translated_field(
+    obj, obj_field_name, value, max_length=None, clean=True
+):
+    obj._changed |= set_tku_translated_field(
+        obj, obj_field_name, value, max_length, clean
+    )
 
 
 def postcodes():
-    path = os.path.join(settings.BASE_DIR, 'data', 'fi', 'postcodes.txt')
+    path = os.path.join(settings.BASE_DIR, "data", "fi", "postcodes.txt")
     _postcodes = {}
-    f = open(path, 'r', encoding='utf-8')
+    f = open(path, "r", encoding="utf-8")
     for l in f.readlines():
-        code, muni = l.split(',')
+        code, muni = l.split(",")
         _postcodes[code] = muni.strip()
     return _postcodes
 
 
 def nl2br(string):
-    return '<br>'.join(string.splitlines())
+    return "<br>".join(string.splitlines())
 
 
-def get_weekday_str(index, lang='fi'):
-    assert 1 <= index <= 7 and lang in ('fi', 'sv', 'en')
+def get_weekday_str(index, lang="fi"):
+    assert 1 <= index <= 7 and lang in ("fi", "sv", "en")
     weekdays = (
-        ('ma', 'mån', 'Mon'),
-        ('ti', 'tis', 'Tue'),
-        ('ke', 'ons', 'Wed'),
-        ('to', 'tor', 'Thu'),
-        ('pe', 'fre', 'Fri'),
-        ('la', 'lör', 'Sat'),
-        ('su', 'sön', 'Sun'),
+        ("ma", "mån", "Mon"),
+        ("ti", "tis", "Tue"),
+        ("ke", "ons", "Wed"),
+        ("to", "tor", "Thu"),
+        ("pe", "fre", "Fri"),
+        ("la", "lör", "Sat"),
+        ("su", "sön", "Sun"),
     )
-    return weekdays[index - 1][['fi', 'sv', 'en'].index(lang)]
+    return weekdays[index - 1][["fi", "sv", "en"].index(lang)]
 
 
-def get_localized_value(data, preferred_language='fi'):
-    assert preferred_language in ('fi', 'sv', 'en')
-    return data.get(preferred_language) or ''
+def get_localized_value(data, preferred_language="fi"):
+    assert preferred_language in ("fi", "sv", "en")
+    return data.get(preferred_language) or ""
 
 
 def convert_code_to_int(code):
     if code:
-        return int.from_bytes(code.encode(), 'big')
+        return int.from_bytes(code.encode(), "big")
     return None
