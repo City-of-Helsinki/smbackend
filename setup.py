636a05f2d471e24616c43ef3fe5d9ce046391132
Mika Hietanen <m.hietanen84@gmail.com>
2020-09-14 13:00:37 +0300

    Update project and munigeo versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="smbackend",
-    version="200910",
+    version="200914",
     license="AGPLv3",
     packages=find_packages(),
     include_package_data=True,
