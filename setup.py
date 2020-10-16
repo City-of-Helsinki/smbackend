753283b4ee38c196e93699d501f4697c14372aa2
Mika Hietanen <m.hietanen84@gmail.com>
2020-10-16 10:22:06 +0300

    Update project version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="smbackend",
-    version="201002",
+    version="201016",
     license="AGPLv3",
     packages=find_packages(),
     include_package_data=True,
