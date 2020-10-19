3ea8a7a2b93ec37ac8a9d14c31148a8e59fa13d7
Mika Hietanen <m.hietanen84@gmail.com>
2020-10-19 12:54:21 +0300

    Update fi/en/sv-translations
    Update project version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="smbackend",
-    version="201016b",
+    version="201019",
     license="AGPLv3",
     packages=find_packages(),
     include_package_data=True,
