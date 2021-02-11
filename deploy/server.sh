409e4f6811bb58c4cf3ea38755b15b21637c6cab
Ville Koivunen <ville.koivunen@hel.fi>
2021-02-11 13:08:42 +0200

    Tune Docker image for server side and include newer GDAL (Ubuntu 20.04)

--- deploy/server.sh
+++ deploy/server.sh
@@ -4,4 +4,4 @@ echo "NOTICE: Get static files for serving"
 ./manage.py collectstatic --no-input
 
 echo "NOTICE: Start the uwsgi web server"
-exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --check-static /usr/src/app/www
+exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --static-map /static=/srv/smbackend/static
--- deploy/settings.py
+++ deploy/settings.py
@@ -11,15 +11,3 @@ except ValueError:
 smbackend.settings.MIDDLEWARE.insert(
     place, "whitenoise.middleware.WhiteNoiseMiddleware"
 )
-
-
-deploy_env = smbackend.settings.environ.Env(
-    USE_X_FORWARDED_HOST=(bool, False),
-    SECURE_PROXY=(bool, False),
-    MEDIA_ROOT=(str, "/usr/src/app/www"),
-)
-
-USE_X_FORWARDED_HOST = deploy_env("USE_X_FORWARDED_HOST")
-
-if deploy_env("SECURE_PROXY"):
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
