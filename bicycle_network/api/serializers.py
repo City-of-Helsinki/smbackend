87c1db0f7bedfc70cee27c8700a43b5bf74247e8
juuso-j <juuso.jokiniemi@turku.fi>
2021-12-10 11:58:38 +0200

    Now merges linestrings that are connected, also support for multilinestrings

--- bicycle_network/api/serializers.py
+++ bicycle_network/api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import LineString, MultiLineString
 from rest_framework import serializers
 from ..models import (
     BicycleNetwork,
@@ -23,13 +24,22 @@ class BicycleNetworkPartCoordsSerializer(serializers.ModelSerializer):
     def get_geometry_coords(self, obj):
         if obj.geometry:
             if self.context["latlon"] == True:
-                # Return LineString coordinates in (lat,lon) format
-                coords = []
-                for coord in obj.geometry.coords:
-                    # swap lon,lat -> lat lon
-                    e=(coord[1],coord[0])
-                    coords.append(e)                
-                return coords
+                if isinstance(obj.geometry, LineString):      
+                    # Return LineString coordinates in (lat,lon) format
+                    coords = []
+                    for coord in obj.geometry.coords:
+                        # swap lon,lat -> lat lon
+                        e=(coord[1],coord[0])
+                        coords.append(e)                
+                    return coords
+                elif isinstance(obj.geometry, MultiLineString):
+                    coords = []
+                    for linestring in obj.geometry.coords:
+                        # swap lon,lat -> lat lon
+                        for coord in linestring:
+                            e=(coord[1],coord[0])
+                            coords.append(e)                
+                    return coords
             else:
                 return obj.geometry.coords
             
