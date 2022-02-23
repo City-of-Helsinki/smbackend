e45af736e5dcbb8aefcba6eb5fc1aff04d4f3706
juuso-j <juuso.jokiniemi@turku.fi>
2022-02-23 08:02:09 +0200

    Pagination now works correctly. Serializes now the objects into same results list with a object_type field.

--- services/search/tests/test_api.py
+++ services/search/tests/test_api.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework.reverse import reverse
 
+
 @pytest.mark.django_db
 def test_search(
     api_client, units, streets, services, addresses, administrative_division
@@ -10,27 +11,30 @@ def test_search(
     # trigram search is not used.
 
     # Search only units and services, no munigeo data in fixtures
-    url = reverse("search") + "?q=museo&use_trigram=false"
+    url = reverse("search") + "?q=museo&use_trigram=false&extended_serializer=false"
     response = api_client.get(url)
     assert response.status_code == 200
     results = response.json()["results"]
-    assert len(results["units"]) == 2
-    assert len(results["services"]) == 1
-    assert len(results["addresses"]) == 0
-    assert len(results["administrative_divisions"]) == 0
+    assert results[0]["object_type"] == "unit"
+    assert results[0]["name"]["fi"] == "Museo"
+    assert results[2]["object_type"] == "service"
+    assert results[2]["name"]["sv"] == "Museum"
+
     # Test that unit Impivara is retrived from service Uimahalli
-    url = reverse("search") + "?q=uimahalli&use_trigram=false"
+    url = reverse("search") + "?q=uimahalli&use_trigram=false&extended_serializer=false"
     response = api_client.get(url)
     results = response.json()["results"]
-    assert results["units"][0]["name"]["fi"] == "Impivaara"
-    assert results["services"][0]["name"]["fi"] == "Uimahalli"
+    assert results[0]["name"]["fi"] == "Impivaara"
+    assert results[1]["name"]["fi"] == "Uimahalli"
     # Test address search
     url = reverse("search") + "?q=kurra&use_trigram=false"
     response = api_client.get(url)
     results = response.json()["results"]
-    assert results["addresses"][0]["full_name"]["fi"] == "Kurrapolku 1A"
+    assert results[0]["full_name"]["fi"] == "Kurrapolku 1A"
+    assert results[0]["object_type"] == "address"
     # Test administrative division search
-    url = reverse("search") + "?q=tur&use_trigram=false"
+    url = reverse("search") + "?q=tur&use_trigram=false&extended_serializer=false"
     response = api_client.get(url)
     results = response.json()["results"]
-    assert results["administrative_divisions"][0]["name"]["fi"] == "Turku"
+    assert results[0]["object_type"] == "administrativedivision"
+    assert results[0]["name"]["fi"] == "Turku"
