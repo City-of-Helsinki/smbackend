c8bc186b397656f06ad91eb14de7e4b6613af38a
juuso-j <juuso.jokiniemi@turku.fi>
2022-03-04 08:16:56 +0200

    Updated test to reflect the changed ordering of results.
    Results are ordered now descending by created field. Changed the order of asserts to reflect this changed.

--- iot/tests/test_api.py
+++ iot/tests/test_api.py
@@ -1,7 +1,7 @@
 import pytest
-from rest_framework.reverse import reverse
 from django.conf import settings
 from django.test import override_settings
+from rest_framework.reverse import reverse
 
 
 @override_settings(CACHES=settings.TEST_CACHES)
@@ -11,5 +11,5 @@ def test_api(api_client, iot_data, iot_data_source):
     response = api_client.get(url)
     assert response.status_code == 200
     results = response.json()["results"]
-    assert results[0]["data"] == {"test": 42}
-    assert results[1]["data"] == {"Even more test": "Data"}
+    assert results[0]["data"] == {"Even more test": "Data"}
+    assert results[1]["data"] == {"test": 42}
