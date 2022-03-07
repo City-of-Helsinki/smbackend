564e9b1ff81a7557f4d69a40176b45de2356d482
juuso-j <juuso.jokiniemi@turku.fi>
2022-03-07 15:58:10 +0200

    Populate multilingual search_columns

--- services/management/commands/index_search_columns.py
+++ services/management/commands/index_search_columns.py
@@ -1,23 +1,22 @@
 import logging
-from django.core.management.base import BaseCommand
+
 from django.contrib.postgres.search import SearchVector
+from django.core.management.base import BaseCommand
 from munigeo.models import Address, AdministrativeDivision
-from services.models import (
-    Service,
-    Unit,
-)
+
+from services.models import Service, Unit
 
 logger = logging.getLogger("search")
 
 
-def get_search_column(model):
+def get_search_column(model, lang):
     """
     Reads the columns, config languages and weights from the model
     to be indexed. Creates and returns a CombinedSearchVector, that
     can be stored into the search_column.
     """
     search_column = None
-    columns = model.get_search_column_indexing()
+    columns = model.get_search_column_indexing(lang)
     for column in columns:
         if search_column:
             search_column += SearchVector(column[0], config=column[1], weight=column[2])
@@ -30,15 +29,18 @@ def get_search_column(model):
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
-        logger.info(
-            f"Units indexed: {Unit.objects.update(search_column=get_search_column(Unit))}"
-        )
-        logger.info(
-            f"Services indexed: {Service.objects.update(search_column=get_search_column(Service))}"
-        )
-        logger.info(
-            f"AdministrativeDivisions indexed: {AdministrativeDivision.objects.update(search_column=get_search_column(AdministrativeDivision))}"
-        )
-        logger.info(
-            f"Addresses indexed: {Address.objects.update(search_column=get_search_column(Address))}"
-        )
+        for lang in ["fi", "sv", "en"]:
+            key = "search_column_%s" % lang
+            logger.info(
+                f"{lang} Units indexed: {Unit.objects.update(**{key: get_search_column(Unit, lang)})}"
+            )
+            logger.info(
+                f"{lang} Services indexed: {Service.objects.update(**{key: get_search_column(Service, lang)})}"
+            )   
+            adm_str = f"{lang} AdministrativeDivisions indexed: " 
+            logger.info(
+                f"{adm_str}{AdministrativeDivision.objects.update(**{key: get_search_column(AdministrativeDivision, lang)})}"
+            )
+            logger.info(
+                f"{lang} Addresses indexed: {Address.objects.update(**{key: get_search_column(Address, lang)})}"
+            )
--- services/signals.py
+++ services/signals.py
@@ -1,51 +1,62 @@
-from functools import reduce
 import operator
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+from functools import reduce
+
 from django.contrib.postgres.search import SearchVector
 from django.db import transaction
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from munigeo.models import Address, AdministrativeDivision
-from services.models import (
-    Unit,
-    Service,
-)
+
+from services.models import Service, Unit
 
 
 @receiver(post_save, sender=Unit)
 def unit_on_save(sender, **kwargs):
-    obj = kwargs["instance"]    
+    obj = kwargs["instance"]
     # Do transaction after successfull commit.
     transaction.on_commit(populate_search_column(obj))
 
+
 @receiver(post_save, sender=Service)
 def service_on_save(sender, **kwargs):
-    obj = kwargs["instance"]    
+    obj = kwargs["instance"]
     transaction.on_commit(populate_search_column(obj))
+
 
 @receiver(post_save, sender=Address)
 def address_on_save(sender, **kwargs):
     obj = kwargs["instance"]
     transaction.on_commit(populate_search_column(obj))
 
+
 @receiver(post_save, sender=AdministrativeDivision)
 def administrative_division_on_save(sender, **kwargs):
     obj = kwargs["instance"]
     transaction.on_commit(populate_search_column(obj))
 
+
 def populate_search_column(obj):
     # Get the information of columns and weights to be added to sear from the model
-    columns = obj.get_search_column_indexing()
+    columns = {}
+    columns["fi"] = obj.get_search_column_indexing("fi")
+    columns["sv"] = obj.get_search_column_indexing("sv")
+    columns["en"] = obj.get_search_column_indexing("en")
+
     id = obj.id
+
     def on_commit():
-        search_vectors = []
-        for column in columns:
-            search_vectors.append(
-                SearchVector(column[0], config=column[1], weight=column[2])
+        search_vectors = {}
+        for lang in ["fi", "sv", "en"]:
+            search_vectors[lang] = []
+            for column in columns[lang]:
+                search_vectors[lang].append(
+                    SearchVector(column[0], config=column[1], weight=column[2])
+                )
+
+            # Add all SearchVectors in search_vectors list to search_column.
+            key = "search_column_%s" % lang
+            obj.__class__.objects.filter(id=id).update(
+                **{key: reduce(operator.add, search_vectors[lang])}
             )
-        # Add all SearchVectors in searc_vectors list to search_column.      
-        obj.__class__.objects.filter(id=id).update(
-            search_column=reduce(operator.add, search_vectors)
-        )
+
     return on_commit
-
-
