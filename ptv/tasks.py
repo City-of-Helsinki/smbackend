0a6621a6da34fada36d80edcf98ad534acb1c4e0
juuso-j <juuso.jokiniemi@turku.fi>
2022-03-09 17:02:55 +0200

    Add Celery task that imports Southwest Finland

---
+++ ptv/tasks.py
@@ -0,0 +1,35 @@
+from celery import shared_task
+from django.core import management
+
+
+@shared_task
+def import_ptv_data(name="import_ptv_data"):
+    # Note, Aura=19 has been removed, thus it is not found in palvelutietovaranto.
+    management.call_command(
+        "ptv_import",
+        "202",
+        "284",
+        "304",
+        "322",
+        "400",
+        "423",
+        "430",
+        "445",
+        "480",
+        "481",
+        "529",
+        "538",
+        "561",
+        "577",
+        "631",
+        "636",
+        "680",
+        "704",
+        "734",
+        "738",
+        "761",
+        "833",
+        "853",
+        "895",
+        "918",
+    )
