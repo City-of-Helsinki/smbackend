f2d35088294917c102783f4d872bceae8e34ab92
juuso-j <juuso.jokiniemi@turku.fi>
2022-03-11 11:31:13 +0200

    Index ServiceNodes
    Indexes ServiceNode that have the service_reference set to None.

--- services/management/commands/index_search_columns.py
+++ services/management/commands/index_search_columns.py
@@ -4,7 +4,7 @@ from django.contrib.postgres.search import SearchVector
 from django.core.management.base import BaseCommand
 from munigeo.models import Address, AdministrativeDivision
 
-from services.models import Service, Unit
+from services.models import Service, ServiceNode, Unit
 
 logger = logging.getLogger("search")
 
@@ -26,6 +26,32 @@ def get_search_column(model, lang):
     return search_column
 
 
+def index_servicenodes(lang):
+    """
+    Index ServiceNodes which service_reference is null
+    to avoid duplicates with Services in results
+    """
+    service_nodes_indexed = 0
+    key = "search_column_%s" % lang
+    columns = ServiceNode.get_search_column_indexing(lang)
+    for service_node in ServiceNode.objects.all():
+        search_column = None
+        if service_node.service_reference is None:
+            for column in columns:
+                if search_column:
+                    search_column += SearchVector(
+                        column[0], config=column[1], weight=column[2]
+                    )
+                else:
+                    search_column = SearchVector(
+                        column[0], config=column[1], weight=column[2]
+                    )
+            setattr(service_node, key, search_column)
+            service_node.save()
+            service_nodes_indexed += 1
+    return service_nodes_indexed
+
+
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
@@ -37,6 +63,8 @@ class Command(BaseCommand):
             logger.info(
                 f"{lang} Services indexed: {Service.objects.update(**{key: get_search_column(Service, lang)})}"
             )
+            logger.info(f"{lang} ServiceNodes indexed: {index_servicenodes(lang)}")
+
             logger.info(
                 f"{lang} AdministrativeDivisions indexed: "
                 f"{AdministrativeDivision.objects.update(**{key: get_search_column(AdministrativeDivision, lang)})}"
