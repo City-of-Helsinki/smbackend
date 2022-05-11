6481f06e00fd75c1690ed83f857a5a1612266eb8
juuso-j <juuso.jokiniemi@turku.fi>
2022-05-11 12:58:50 +0300

    Use superclass BaseImportCommand and test data if run in test mode

--- mobility_data/management/commands/import_payment_zones.py
+++ mobility_data/management/commands/import_payment_zones.py
@@ -1,18 +1,24 @@
 import logging
 
-from django.core.management import BaseCommand
-
 from mobility_data.importers.payment_zones import (
     get_payment_zone_objects,
     save_to_database,
 )
 
+from ._base_import_command import BaseImportCommand
+from ._utils import get_test_gdal_data_source
+
 logger = logging.getLogger("mobility_data")
 
 
-class Command(BaseCommand):
+class Command(BaseImportCommand):
     def handle(self, *args, **options):
         logger.info("Importing payment zones.")
-        objects = get_payment_zone_objects()
+        if options["test_mode"]:
+            logger.info("Running payment_zones importer in test mode.")
+            data_source = get_test_gdal_data_source(options["test_mode"])
+            objects = get_payment_zone_objects(data_source=data_source)
+        else:
+            objects = get_payment_zone_objects()
         save_to_database(objects)
         logger.info(f"Saved {len(objects)} payment zones.")
