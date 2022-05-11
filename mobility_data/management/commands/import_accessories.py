a32b868cfb71ead56bcf125cf2fcc7b2ecdb5179
juuso-j <juuso.jokiniemi@turku.fi>
2022-05-11 12:06:13 +0300

    Use superclass BaseImportCommand, use test data if run in test mode

--- mobility_data/management/commands/import_accessories.py
+++ mobility_data/management/commands/import_accessories.py
@@ -1,20 +1,24 @@
 import logging
 
-from django.core.management import BaseCommand
-
 from mobility_data.importers.accessories import (
     get_accessory_elements,
     get_accessory_objects,
     save_to_database,
 )
 
+from ._base_import_command import BaseImportCommand
+from ._utils import get_test_gdal_data_source
+
 logger = logging.getLogger("mobility_data")
 
 
-class Command(BaseCommand):
+class Command(BaseImportCommand):
     def handle(self, *args, **options):
         logger.info("Importing accessories...")
-        accessories = get_accessory_objects()
+        data_source = None
+        if options["test_mode"]:
+            data_source = get_test_gdal_data_source(options["test_mode"])
+        accessories = get_accessory_objects(data_source=data_source)
         for key, value in get_accessory_elements().items():
             objects = getattr(accessories, value["objects_field_name"], None)
             create_content_type_func = value["create_content_type_func"]
