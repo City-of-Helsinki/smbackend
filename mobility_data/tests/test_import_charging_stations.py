67e0f33b60b865898d9de2ab7272f7e2f3b86d04
juuso-j <juuso.jokiniemi@turku.fi>
2022-05-11 13:16:50 +0300

    Use import_command function to run importer

--- mobility_data/tests/test_import_charging_stations.py
+++ mobility_data/tests/test_import_charging_stations.py
@@ -1,21 +1,9 @@
-from io import StringIO
 import pytest
-from django.core.management import call_command
-from mobility_data.models import (
-    MobileUnit,
-    ContentType,
-)
 
-def import_command(*args, **kwargs):
-        out = StringIO()
-        call_command(
-            "import_charging_stations",
-            *args,
-            stdout=out,
-            stderr=StringIO(),
-            **kwargs,
-        )
-        return out.getvalue()
+from mobility_data.models import ContentType, MobileUnit
+
+from .utils import import_command
+
 
 @pytest.mark.django_db
 def test_importer(
@@ -23,11 +11,18 @@ def test_importer(
     administrative_division_type,
     administrative_division,
     streets,
-    address
-    ):
-    out = import_command(test_mode="charging_stations.json")
-    assert ContentType.objects.filter(type_name=ContentType.CHARGING_STATION).count() == 1
-    assert MobileUnit.objects.filter(content_type__type_name=ContentType.CHARGING_STATION).count() == 2
+    address,
+):
+    import_command("import_charging_stations", test_mode="charging_stations.json")
+    assert (
+        ContentType.objects.filter(type_name=ContentType.CHARGING_STATION).count() == 1
+    )
+    assert (
+        MobileUnit.objects.filter(
+            content_type__type_name=ContentType.CHARGING_STATION
+        ).count()
+        == 2
+    )
     unit = MobileUnit.objects.get(name="AimoPark Stockmann Turku")
     assert unit.address_fi == "Kristiinankatu 11, 20100 Turku"
     assert unit.address_sv == "Kristinegatan 11, 20100 Turku"
@@ -35,5 +30,5 @@ def test_importer(
     assert unit
     # Transform to source data srid
     unit.geometry.transform(4326)
-    assert pytest.approx(unit.geometry.x, 0.0001) == 22.247    
+    assert pytest.approx(unit.geometry.x, 0.0001) == 22.247
     assert unit.extra["url"] == "https://latauskartta.fi/latauspiste/2629/Hotel+Kakola/"
