c3e1fa7fbb8906aebc5de893878cbefa9975d13c
juuso-j <juuso.jokiniemi@turku.fi>
2022-05-11 13:18:17 +0300

    Use import_command function to run importer

--- mobility_data/tests/test_import_gas_filling_stations.py
+++ mobility_data/tests/test_import_gas_filling_stations.py
@@ -1,32 +1,28 @@
-from io import StringIO
 import pytest
-from django.core.management import call_command
-from mobility_data.models import (
-    MobileUnit,
-    ContentType,
-)
 
-def import_command(*args, **kwargs):
-        out = StringIO()
-        call_command(
-            "import_gas_filling_stations",
-            *args,
-            stdout=out,
-            stderr=StringIO(),
-            **kwargs,
-        )
-        return out.getvalue()
+from mobility_data.models import ContentType, MobileUnit
+
+from .utils import import_command
+
 
 @pytest.mark.django_db
 def test_importer():
-    out = import_command(test_mode="gas_filling_stations.json")
-    assert ContentType.objects.filter(type_name=ContentType.GAS_FILLING_STATION).count() == 1
-    assert MobileUnit.objects.filter(content_type__type_name=ContentType.GAS_FILLING_STATION).count() == 2
+    import_command("import_gas_filling_stations", test_mode="gas_filling_stations.json")
+    assert (
+        ContentType.objects.filter(type_name=ContentType.GAS_FILLING_STATION).count()
+        == 1
+    )
+    assert (
+        MobileUnit.objects.filter(
+            content_type__type_name=ContentType.GAS_FILLING_STATION
+        ).count()
+        == 2
+    )
     assert MobileUnit.objects.get(name="Raisio Kuninkoja")
     unit = MobileUnit.objects.get(name="Turku Satama")
     assert unit
     assert unit.address == "Tuontiväylä 42 abc 1-2, 20200 Turku"
     # Transform to source data srid
     unit.geometry.transform(3857)
-    assert pytest.approx(unit.geometry.x, 0.0000000001) ==  2472735.3962113541
+    assert pytest.approx(unit.geometry.x, 0.0000000001) == 2472735.3962113541
     assert unit.extra["operator"] == "Gasum"
