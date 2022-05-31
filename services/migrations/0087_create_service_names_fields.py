d24aacdd0629878ab197580d48e310ee856343b3
juuso-j <juuso.jokiniemi@turku.fi>
2022-05-31 11:05:52 +0300

    Pass Flake8 style guide enforcement

--- services/migrations/0087_create_service_names_fields.py
+++ services/migrations/0087_create_service_names_fields.py
@@ -7,23 +7,29 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('services', '0086_unit_related_units'),
+        ("services", "0086_unit_related_units"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='unit',
-            name='service_names_en',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=200), default=list, size=None),
+            model_name="unit",
+            name="service_names_en",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(max_length=200), default=list, size=None
+            ),
         ),
         migrations.AddField(
-            model_name='unit',
-            name='service_names_fi',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=200), default=list, size=None),
+            model_name="unit",
+            name="service_names_fi",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(max_length=200), default=list, size=None
+            ),
         ),
         migrations.AddField(
-            model_name='unit',
-            name='service_names_sv',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=200), default=list, size=None),
+            model_name="unit",
+            name="service_names_sv",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(max_length=200), default=list, size=None
+            ),
         ),
     ]
