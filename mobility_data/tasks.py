c6a25bf649356cc23c13d4fe700828e96957553b
juuso-j <juuso.jokiniemi@turku.fi>
2022-06-17 10:08:12 +0300

    Add task that imports marinas

--- mobility_data/tasks.py
+++ mobility_data/tasks.py
@@ -43,3 +43,8 @@ def import_share_car_parking_places(name="impor_share_car_parking_places"):
 @shared_task
 def import_bicycle_networks(name="import_bicycle_networks"):
     management.call_command("import_bicycle_networks")
+
+
+@shared_task
+def import_marinas(name="import_marinas"):
+    management.call_command("import_marinas")
