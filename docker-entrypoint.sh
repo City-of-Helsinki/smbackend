01e95b35313faeb829c04798d72092e817cec681
juuso-j <juuso.jokiniemi@turku.fi>
2022-07-15 07:26:14 +0300

    Create superuser if not exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 set -e
-
 if [[ "$APPLY_MIGRATIONS" = "true" ]]; then
     echo "Applying database migrations..."
     ./manage.py migrate --noinput
 fi
 
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    echo "Creating superuser if not exists."
+    python manage.py ensure_adminuser --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL \
+        --password $DJANGO_SUPERUSER_PASSWORD
+fi
+
 if [ "$1" = 'start_django_development_server' ]; then
     # Start server
-    echo "Starting development server"
+    echo "Starting development server."
     ./manage.py runserver 0.0.0.0:8000
 
 elif [ "$1" = 'maintenance_tasks' ]; then
