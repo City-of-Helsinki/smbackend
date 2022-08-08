593390e3f033838bab2f41bef304df5bff014be5
Mika Hietanen <m.hietanen84@gmail.com>
2022-08-08 14:00:17 +0300

    Add API tests for outdoor sports map usage

--- services/tests/test_notifications_api.py
+++ services/tests/test_notifications_api.py
@@ -22,6 +22,14 @@ def create_notifications(model):
     model.objects.create(
         title="Hidden notification", content="About the alert...", active=False
     )
+    model.objects.create(
+        title_fi="Ulkoliikuntakartan ilmoitus",
+        title_en="Outdoor sports map notification",
+        content_fi="Ulkoliikuntakartan sisältö",
+        content_en="Outdoor sports map content",
+        active=True,
+        outdoor_sports_map_usage=True,
+    )
 
 
 @pytest.fixture
@@ -71,3 +79,32 @@ def test_get_notification_list(api_client, notification_model, endpoint):
         "sv": "Exempel på extern URL titel",
     }
     assert results[0].get("picture_url") == "https://example.com/picture"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "notification_model, endpoint",
+    [
+        (Announcement, "announcement-list"),
+        (ErrorMessage, "errormessage-list"),
+    ],
+)
+def test_get_outdoor_sports_map_notification_list(
+    api_client, notification_model, endpoint
+):
+    create_notifications(model=notification_model)
+    response = get(
+        api_client, reverse(endpoint), data={"outdoor_sports_map_usage": True}
+    )
+    results = response.data["results"]
+
+    assert response.status_code == 200
+    assert response.data["count"] == 1
+    assert results[0].get("title") == {
+        "fi": "Ulkoliikuntakartan ilmoitus",
+        "en": "Outdoor sports map notification",
+    }
+    assert results[0].get("content") == {
+        "fi": "Ulkoliikuntakartan sisältö",
+        "en": "Outdoor sports map content",
+    }
