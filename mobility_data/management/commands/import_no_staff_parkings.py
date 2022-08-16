e46125632731f1d0ef56de66ef93c77a0910371e
juuso-j <juuso.jokiniemi@turku.fi>
2022-08-16 10:48:03 +0300

    Will pass test fixture as argument

--- mobility_data/management/commands/import_no_staff_parkings.py
+++ mobility_data/management/commands/import_no_staff_parkings.py
@@ -13,6 +13,9 @@ logger = logging.getLogger("mobility_data")
 class Command(BaseImportCommand):
     def handle(self, *args, **options):
         logger.info("Importing no staff parkings.")
-        objects = get_no_staff_parking_objects()
+        geojson_file = None
+        if options["test_mode"]:
+            geojson_file = options["test_mode"]
+        objects = get_no_staff_parking_objects(geojson_file=geojson_file)
         save_to_database(objects)
         logger.info(f"Imorted {len(objects)} no staff parkings")
