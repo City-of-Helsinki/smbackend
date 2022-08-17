649d67dd72011fe3d64c6af980caa79911f709ed
juuso-j <juuso.jokiniemi@turku.fi>
2022-08-17 10:26:49 +0300

    Add retrieving of units by address

--- services/api.py
+++ services/api.py
@@ -998,6 +998,9 @@ class UnitViewSet(
                 | Q(service_nodes__in=service_nodes_by_ancestors(servicenode_ids))
             ).distinct()
 
+        if "address" in filters:
+            queryset = queryset.filter(street_address__contains=filters["address"])
+
         maintenance_organization = self.request.query_params.get(
             "maintenance_organization"
         )
