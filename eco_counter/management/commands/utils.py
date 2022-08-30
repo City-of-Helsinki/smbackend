5814afbae2e78d49be2814883a568b8d9202382f
juuso-j <juuso.jokiniemi@turku.fi>
2022-08-30 14:35:31 +0300

    Builds correct formated csv, refactoring etc...

--- eco_counter/management/commands/utils.py
+++ eco_counter/management/commands/utils.py
@@ -8,46 +8,33 @@ from django.conf import settings
 from django.contrib.gis.gdal import DataSource as DataSource
 from django.contrib.gis.geos import GEOSGeometry, Point
 
-from eco_counter.models import ECO_COUNTER, Station, TRAFFIC_COUNTER
+from eco_counter.models import (
+    ECO_COUNTER,
+    Station,
+    TRAFFIC_COUNTER,
+    TRAFFIC_COUNTER_END_YEAR,
+    TRAFFIC_COUNTER_START_YEAR,
+)
 from mobility_data.importers.utils import get_root_dir
 
 logger = logging.getLogger("eco_counter")
 
-TRAFFIC_COUNTER_META_DATA_GEOJSON = "meta_data.geojson"
+TRAFFIC_COUNTER_METADATA_GEOJSON = "traffic_counter_metadata.geojson"
 
-TRAFFIC_COUNTER_BASE_URL = "https://data.turku.fi/"
-TRAFFIC_COUNTER_START_YEAR = "2015"
-TRAFFIC_COUNTER_START_YEAR_URL = (
-    TRAFFIC_COUNTER_BASE_URL + "2yxpk2imqi2mzxpa6e6knq/2015_laskenta.csv"
+
+# Create a dict where the years to be importer are keys and the value is the url of the csv data.
+# e.g. {2015, "https://data.turku.fi/2yxpk2imqi2mzxpa6e6knq/2015_laskenta.csv"}
+keys = [k for k in range(TRAFFIC_COUNTER_START_YEAR, TRAFFIC_COUNTER_END_YEAR + 1)]
+TRAFFIC_COUNTER_CSV_URLS = dict(
+    [
+        (k, f"{settings.TRAFFIC_COUNTER_OBSERVATIONS_BASE_URL}{k}_laskenta.csv")
+        for k in keys
+    ]
 )
-TRAFFIC_COUNTER_CSV_URLS = {
-    "2015": {"url": TRAFFIC_COUNTER_START_YEAR_URL},
-    "2016": {
-        "url": TRAFFIC_COUNTER_BASE_URL + "2yxpk2imqi2mzxpa6e6knq/2016_laskenta.csv"
-    },
-    "2017": {
-        "url": TRAFFIC_COUNTER_BASE_URL + "2yxpk2imqi2mzxpa6e6knq/2017_laskenta.csv"
-    },
-    "2018": {
-        "url": TRAFFIC_COUNTER_BASE_URL + "2yxpk2imqi2mzxpa6e6knq/2018_laskenta.csv"
-    },
-    "2019": {
-        "url": TRAFFIC_COUNTER_BASE_URL + "2yxpk2imqi2mzxpa6e6knq/2019_laskenta.csv"
-    },
-    "2020": {
-        "url": TRAFFIC_COUNTER_BASE_URL + "2yxpk2imqi2mzxpa6e6knq/2020_laskenta.csv"
-    },
-    "2021": {
-        "url": TRAFFIC_COUNTER_BASE_URL + "2yxpk2imqi2mzxpa6e6knq/2021_laskenta.csv"
-    },
-    "2022": {
-        "url": TRAFFIC_COUNTER_BASE_URL + "2yxpk2imqi2mzxpa6e6knq/2022_laskenta.csv"
-    },
-}
 
 
-def get_traffic_counter_meta_data_data_layer():
-    meta_file = f"{get_root_dir()}/eco_counter/data/{TRAFFIC_COUNTER_META_DATA_GEOJSON}"
+def get_traffic_counter_metadata_data_layer():
+    meta_file = f"{get_root_dir()}/eco_counter/data/{TRAFFIC_COUNTER_METADATA_GEOJSON}"
     return DataSource(meta_file)[0]
 
 
@@ -65,66 +52,69 @@ def get_eco_counter_csv():
     return get_dataframe(settings.ECO_COUNTER_OBSERVATIONS_URL)
 
 
-def get_traffic_counter_csv():
-    df = get_dataframe(TRAFFIC_COUNTER_START_YEAR_URL)
-    # Append all the Traffic counter CSV data into one CSV.
+def get_traffic_counter_csv(start_year=2015):
+    df = get_dataframe(TRAFFIC_COUNTER_CSV_URLS[start_year])
+    # Concat the Traffic counter CSV data into one CSV.
     for key in TRAFFIC_COUNTER_CSV_URLS.keys():
-        # Skip start year as it is in the initial dataframe
-        if key in TRAFFIC_COUNTER_START_YEAR:
+        # Skip start year as it is in the initial dataframe, skip also
+        # data from years before the start year.
+        if key <= start_year:
             continue
-        app_df = get_dataframe(TRAFFIC_COUNTER_CSV_URLS[key]["url"])
-        # TODO fix deprecated append
-        df = df.append(app_df)
-    data_layer = get_traffic_counter_meta_data_data_layer()
+        app_df = get_dataframe(TRAFFIC_COUNTER_CSV_URLS[key])
+        # ignore_index=True, do not use the index values along the concatenation axis.
+        # The resulting axis will be labeled 0, …, n - 1.
+        df = pd.concat([df, app_df], ignore_index=True)
+
+    data_layer = get_traffic_counter_metadata_data_layer()
+
     ids_not_found = 0
-    # Rename columns to format name_type|direction
+    # Rename columns using the metadata to format: name_type|direction
     # e.g. Yliopistonkatu AK
     for feature in data_layer:
         id = feature["Mittauspisteiden_ID"].as_int()
         direction = feature["Suunta"].as_string()
+        # TODO, remove when the final/corrected version of the metadata is available
         direction = "K"
         measurement_type = feature["Tyyppi"].as_string()
-        # with the id find the col  from csv
+        # with the id find the column from the data csv
         name = feature["Osoite_fi"].as_string()
         regex = rf".*\({id}\)"
         column = df.filter(regex=regex)
         if len(column.keys()) > 1:
-            # do some error handling
-            pass
+            logger.error(f"Multiple ids: {id}, found in csv data, skipping.")
+            continue
         if len(column.keys()) == 0:
-            logger.warning(f"ID:{id} not found in the csv data")
+            logger.warning(f"ID:{id} in metadata not found in csv data")
             ids_not_found += 1
             continue
         col_name = column.keys()[0]
-        # new_name = f"{addr_fi}_{id} {measurement_type}{direction}"
         new_name = f"{name} {measurement_type}{direction}"
-        # Rename the column
+        # Rename the column with the new name that is built from the metadata.
         df.columns = df.columns.str.replace(col_name, new_name, regex=False)
     # drop columns with number, i.e. not in metadata as the new column
-    # name are in format name_type|direction and does not contain numbers
+    # names are in format name_type|direction and does not contain numbers
     df = df.drop(df.filter(regex="[0-9]+").columns, axis=1)
-    print(len(df.columns))
     # Combine columns with same name, i.e. combines lanes into one.
+    # axis=1, split along columns.
     df = df.groupby(df.columns, axis=1).sum()
-    print(len(df.columns))
     logger.info(df.info(verbose=False))
     # Move column 'startTime to first (0) position.
     df.insert(0, "startTime", df.pop("startTime"))
-    # df.to_csv("out.csv")
     return df
 
 
 def save_traffic_counter_stations():
-
+    """
+    Saves the stations defined in the metadata to Station table.
+    """
     saved = 0
-    data_layer = get_traffic_counter_meta_data_data_layer()
+    data_layer = get_traffic_counter_metadata_data_layer()
     for feature in data_layer:
         name = feature["Osoite_fi"].as_string()
         name_sv = feature["Osoite_sv"].as_string()
         name_en = feature["Osoite_en"].as_string()
         if Station.objects.filter(name=name).exists():
             continue
-
         station = Station()
         station.name = name
         station.name_sv = name_sv
