152450fee917badb018e61868dd9e03c688981e4
juuso-j <juuso.jokiniemi@turku.fi>
2022-09-20 14:20:00 +0300

    Add street_maintenance app and logger

--- smbackend/settings.py
+++ smbackend/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = [
     "mobility_data.apps.MobilityDataConfig",
     "bicycle_network.apps.BicycleNetworkConfig",
     "iot.apps.IotConfig",
+    "street_maintenance.apps.StreetMaintenanceConfig",
 ]
 
 if env("ADDITIONAL_INSTALLED_APPS"):
@@ -272,6 +273,7 @@ LOGGING = {
         "eco_counter": {"handlers": ["console"], "level": "INFO"},
         "mobility_data": {"handlers": ["console"], "level": "INFO"},
         "bicycle_network": {"handlers": ["console"], "level": "INFO"},
+        "street_maintenance": {"handlers": ["console"], "level": "INFO"},
     },
 }
 
@@ -298,7 +300,7 @@ CACHES = {
         "LOCATION": env("CACHE_LOCATION"),
     }
 }
-# Include extended information. i.e. name of the task etc. otherwise the name will be empty in 
+# Include extended information. i.e. name of the task etc. otherwise the name will be empty in
 # version 2.4.0
 CELERY_RESULT_EXTENDED = True
 
