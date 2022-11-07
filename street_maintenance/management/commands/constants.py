dce27baefda64e418bf433e0a392e22bcbde124d
juuso-j <juuso.jokiniemi@turku.fi>
2022-11-07 10:32:09 +0200

    Add KUNTEC constants

--- street_maintenance/management/commands/constants.py
+++ street_maintenance/management/commands/constants.py
@@ -5,50 +5,63 @@ AUTORI_ROUTES_URL = settings.AUTORI_ROUTES_URL
 AUTORI_VEHICLES_URL = settings.AUTORI_VEHICLES_URL
 AUTORI_CONTRACTS_URL = settings.AUTORI_CONTRACTS_URL
 AUTORI_TOKEN_URL = settings.AUTORI_TOKEN_URL
+
 INFRAROAD_UNITS_URL = (
     "https://infraroad.fluentprogress.fi/KuntoInfraroad/v1/snowplow/query?since=72hours"
 )
 INFRAROAD_WORKS_URL = "https://infraroad.fluentprogress.fi/KuntoInfraroad/v1/snowplow/{id}?history={history_size}"
 
-
-# Events are categorized into main groups
-AURAUS = "Auraus"
-LIUKKAUDENTORJUNTA = "Liukkaudentorjunta"
-PUHTAANAPITO = "Puhtaanapito"
-HIEKANPOISTO = "Hiekanpoisto"
+KUNTEC_KEY = settings.KUNTEC_KEY
+KUNTEC_UNITS_URL = (
+    f"https://mapon.com/api/v1/unit/list.json?key={KUNTEC_KEY}&include=io_din"
+)
+KUNTEC_WORKS_URL = (
+    "https://mapon.com/api/v1/route/list.json?key={key}&from={start}"
+    "&till={end}&include=polyline&unit_id={unit_id}"
+)
+# Events are categorized into flowwoing main groups:
+AURAUS = "auraus"
+LIUKKAUDENTORJUNTA = "liukkaudentorjunta"
+PUHTAANAPITO = "puhtaanapito"
+HIEKANPOISTO = "hiekanpoisto"
 # MUUT is currenlty only for testing purposes, TODO, remove from production
-MUUT = "Muut"
+MUUT = "muut"
 # As data providers have different names for their events, they are mapped
 # with this dict, so that every event that does the same has the same name.
 EVENT_MAPPINGS = {
-    "Auraus": AURAUS,
-    "Auraus ja sohjonpoisto": AURAUS,
-    "Lumen poisajo": AURAUS,
-    "Lumensiirto": AURAUS,
-    "Suolas": LIUKKAUDENTORJUNTA,
-    "Suolaus (sirotinlaite)": LIUKKAUDENTORJUNTA,
-    "Liuossuolaus": LIUKKAUDENTORJUNTA,
-    "Hiekoitus": LIUKKAUDENTORJUNTA,
-    "Hiekoitus (sirotinlaite)": LIUKKAUDENTORJUNTA,
-    "Linjahiekoitus": LIUKKAUDENTORJUNTA,
-    "Pistehiekoitus": LIUKKAUDENTORJUNTA,
-    "Paannejään poisto": LIUKKAUDENTORJUNTA,
-    "Puhtaanapito": PUHTAANAPITO,
-    "Harjaus": PUHTAANAPITO,
-    "Pesu": PUHTAANAPITO,
-    "Harjaus ja sohjonpoisto": PUHTAANAPITO,
-    "Pölynsidonta": PUHTAANAPITO,
-    "Hiekanpoisto": HIEKANPOISTO,
-    "Muut työt": MUUT,
-    "Lisätyö": MUUT,
-    "Viherhoito": MUUT,
-    "Kaivot": MUUT,
-    "Metsätyöt": MUUT,
-    "Rikkakasvien torjunta": MUUT,
+    "auraus": AURAUS,
+    "auraus ja sohjonpoisto": AURAUS,
+    "lumen poisajo": AURAUS,
+    "lumensiirto": AURAUS,
+    "suolas": LIUKKAUDENTORJUNTA,
+    "suolaus (sirotinlaite)": LIUKKAUDENTORJUNTA,
+    "liuossuolaus": LIUKKAUDENTORJUNTA,
+    "hiekoitus": LIUKKAUDENTORJUNTA,
+    "hiekotus": LIUKKAUDENTORJUNTA,
+    "hiekoitus (sirotinlaite)": LIUKKAUDENTORJUNTA,
+    "linjahiekoitus": LIUKKAUDENTORJUNTA,
+    "pistehiekoitus": LIUKKAUDENTORJUNTA,
+    "paannejään poisto": LIUKKAUDENTORJUNTA,
+    "puhtaanapito": PUHTAANAPITO,
+    "harjaus": PUHTAANAPITO,
+    "pesu": PUHTAANAPITO,
+    "harjaus ja sohjonpoisto": PUHTAANAPITO,
+    "pölynsidonta": PUHTAANAPITO,
+    "hiekanpoisto": HIEKANPOISTO,
+    "muut työt": MUUT,
+    "lisätyö": MUUT,
+    "viherhoito": MUUT,
+    "kaivot": MUUT,
+    "metsätyöt": MUUT,
+    "rikkakasvien torjunta": MUUT,
+    "paikkaus": MUUT,
 }
 # The number of works(point data with timestamp and event) to be fetched for every unit.
 INFRAROAD_DEFAULT_WORKS_HISTORY_SIZE = 10000
 # Length of Autori history size in days, max value is 31.
-AUTORI_DEFAULT_WORKS_HISTORY_SIZE = 10
+AUTORI_DEFAULT_WORKS_HISTORY_SIZE = 5
 AUTORI_MAX_WORKS_HISTORY_SIZE = 31
 AUTORI_DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S%z"
+KUNTEC_DEFAULT_WORKS_HISTORY_SIZE = 10
+KUNTEC_MAX_WORKS_HISTORY_SIZE = 31
+KUNTEC_DATE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
