d54288327fc01000baef152f58432c7060fe6810
juuso-j <juuso.jokiniemi@turku.fi>
2022-11-10 10:38:50 +0200

    Add importing of wfs data sources

--- mobility_data/management/commands/import_mobility_data.py
+++ mobility_data/management/commands/import_mobility_data.py
@@ -6,16 +6,15 @@ import logging
 from django.core import management
 from django.core.management import BaseCommand
 
+from mobility_data.management.commands.import_wfs import get_configured_cotent_types
+
 # Names of the mobility_data importers to be include when importing data.
 importers = [
     "culture_routes",
     "gas_filling_stations",
     "charging_stations",
     "bicycle_stands",
-    "payment_zones",
     "speed_limit_zones",
-    "scooter_restrictions",
-    "accessories",
     "bike_service_stations",
     "share_car_parking_places",
     "bicycle_networks",
@@ -23,13 +22,15 @@ importers = [
     "disabled_and_no_staff_parkings",
     "loading_and_unloading_places",
     "lounaistieto_shapefiles",
-    "paavonpolkus",
 ]
+# Read the content types to be imported
+wfs_content_types = get_configured_cotent_types()
 logger = logging.getLogger("mobility_data")
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info("Importing mobility data...")
+        management.call_command("import_wfs", wfs_content_types)
         for importer in importers:
             management.call_command(f"import_{importer}")
