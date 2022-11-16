dca4969b4eb96e44c876e60f29e8788f6eb69265
juuso-j <juuso.jokiniemi@turku.fi>
2022-11-16 10:48:11 +0200

    Remove choice fields

--- mobility_data/models/content_type.py
+++ mobility_data/models/content_type.py
@@ -19,98 +19,10 @@ class BaseType(models.Model):
 
 
 class ContentType(BaseType):
-    """
-    Every MobileUnit has a ContetType, it describes the type,
-    and gives a way to identify MobileUnits by their types. The
-    ContentType is bound to the data source.
-    """
 
-    CHARGING_STATION = "CGS"
-    GAS_FILLING_STATION = "GFS"
-    CULTURE_ROUTE_UNIT = "CRU"
-    CULTURE_ROUTE_GEOMETRY = "CRG"
-    BICYCLE_STAND = "BIS"
-    PAYMENT_ZONE = "PAZ"
-    SPEED_LIMIT_ZONE = "SLZ"
-    SCOOTER_PARKING = "SPG"
-    SCOOTER_SPEED_LIMIT = "SSL"
-    SCOOTER_NO_PARKING = "SNP"
-    ACCESSORY_PUBLIC_TOILET = "APT"
-    ACCESSORY_BENCH = "ABH"
-    ACCESSORY_TABLE = "ATE"
-    ACCESSORY_FURNITURE_GROUP = "AFG"
-    BIKE_SERVICE_STATION = "BSS"
-    SHARE_CAR_PARKING_PLACE = "SCP"
-    # Harjasuolatut pyöratiet
-    BRUSH_SALTED_BICYCLE_NETWORK = "BLB"
-    # Harjahiekoitetut pyöratiet
-    BRUSH_SANDED_BICYCLE_NETWORK = "BND"
-    GUEST_MARINA = "GMA"
-    BOAT_PARKING = "BOK"
-    MARINA = "MAR"
-    MARINA_SOUTHWEST_FINLAND = "MSF"
-    NO_STAFF_PARKING = "NSP"
-    DISABLED_PARKING = "DSP"
-    BERTH = "BER"
-    LOADING_UNLOADING_PLACE = "LUP"
-    SLIPWAY = "SWY"
-    RECREATIONAL_ROUTE = "RCR"
-    FERRY_ROUTE = "FYR"
-    FISHING_SPOT = "FGS"
-    PAAVONPOLKU = "PPU"
-    FTINESS_TRAIL = "FTL"
-    NATURE_TRAIL = "NTL"
-    HIKING_TRAIL = "HTL"
-    PADDLING_TRAIL = "PTL"
-    CONTENT_TYPES = [
-        (CHARGING_STATION, "ChargingStation"),
-        (GAS_FILLING_STATION, "GasFillingStation"),
-        (CULTURE_ROUTE_UNIT, "CultureRouteUnit"),
-        (CULTURE_ROUTE_GEOMETRY, "CultureRouteGeometry"),
-        (BICYCLE_STAND, "BicycleStand"),
-        (PAYMENT_ZONE, "PaymentZone"),
-        (SPEED_LIMIT_ZONE, "SpeedLimitZone"),
-        (SCOOTER_PARKING, "ScooterParking"),
-        (SCOOTER_SPEED_LIMIT, "ScooterSpeedLimit"),
-        (SCOOTER_NO_PARKING, "ScooterNoParking"),
-        (ACCESSORY_PUBLIC_TOILET, "AccessoryPublicToilet"),
-        (ACCESSORY_BENCH, "AccessoryBench"),
-        (ACCESSORY_TABLE, "AccessoryTable"),
-        (ACCESSORY_FURNITURE_GROUP, "AccessoryFurnitureGroup"),
-        (BIKE_SERVICE_STATION, "BikeServiceStation"),
-        (SHARE_CAR_PARKING_PLACE, "ShareCarParkingPlace"),
-        (BRUSH_SALTED_BICYCLE_NETWORK, "BrushSaltedBicycleNetwork"),
-        (BRUSH_SANDED_BICYCLE_NETWORK, "BrushSandedBicycleNetwork"),
-        (GUEST_MARINA, "GuestMarina"),
-        (BOAT_PARKING, "BoatParking"),
-        (MARINA, "Marina"),
-        (MARINA_SOUTHWEST_FINLAND, "MarinaSouthestFinalnd"),
-        (NO_STAFF_PARKING, "NoStaffParking"),
-        (DISABLED_PARKING, "DisabledParking"),
-        (BERTH, "Berth"),
-        (LOADING_UNLOADING_PLACE, "LoadingUnloadingPlace"),
-        (SLIPWAY, "Slipway"),
-        (RECREATIONAL_ROUTE, "RecreationalRoute"),
-        (FERRY_ROUTE, "FerryRoute"),
-        (FISHING_SPOT, "FishingSpot"),
-        (PAAVONPOLKU, "PaavonPolku"),
-        (FTINESS_TRAIL, "FitnessTrail"),
-        (NATURE_TRAIL, "NatureTrail"),
-        (HIKING_TRAIL, "HikingTrail"),
-        (PADDLING_TRAIL, "PaddlingTrail"),
-    ]
-    type_name = models.CharField(max_length=3, choices=CONTENT_TYPES, null=True)
+    type_name = models.CharField(max_length=3, null=True)
 
 
 class GroupType(BaseType):
-    """
-    Every MobileUnitGroup has a GroupType, it descriptes the type,
-    and gives a way to identify MobileUnitGroups by their types.
-    """
 
-    CULTURE_ROUTE = "CRE"
-
-    GROUP_TYPES = [
-        (CULTURE_ROUTE, "CULTURE_ROUTE"),
-    ]
-    type_name = models.CharField(max_length=3, choices=GROUP_TYPES, null=True)
+    type_name = models.CharField(max_length=3, null=True)
