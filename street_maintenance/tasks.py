8d0ea3eb5233e33025e58a6023d8822c7039f959
juuso-j <juuso.jokiniemi@turku.fi>
2023-01-13 11:51:36 +0200

    Change autori to yit

--- street_maintenance/tasks.py
+++ street_maintenance/tasks.py
@@ -15,15 +15,15 @@ def import_infraroad_street_maintenance_history(
 
 
 @shared_task
-def import_autori_street_maintenance_history(
-    args=None, name="import_autori_street_maintenance_history"
+def import_yit_street_maintenance_history(
+    args=None, name="import_yit_street_maintenance_history"
 ):
     if args:
         management.call_command(
-            "import_autori_street_maintenance_history", "--history-size", args
+            "import_yit_street_maintenance_history", "--history-size", args
         )
     else:
-        management.call_command("import_autori_street_maintenance_history")
+        management.call_command("import_yit_street_maintenance_history")
 
 
 @shared_task
