8f57fe6c846d01827ccd5c666109086e420bba72
juuso-j <juuso.jokiniemi@turku.fi>
2023-02-06 12:30:35 +0200

    Replace @shared_task with @shared_task_email decorator

--- smbackend_turku/tasks.py
+++ smbackend_turku/tasks.py
@@ -1,30 +1,31 @@
-from celery import shared_task
 from django.core import management
 
+from smbackend.utils import shared_task_email
 
-@shared_task
+
+@shared_task_email
 def turku_services_import(args, name="turku_services_import"):
     management.call_command("turku_services_import", args)
 
 
-@shared_task
+@shared_task_email
 def delete_external_source(source, name="delete_external_source"):
     management.call_command("turku_services_import", source, "--delete-external-source")
 
 
-@shared_task
+@shared_task_email
 def import_mds_data(name="import_mds_data"):
     management.call_command(
         "turku_services_import", "services", "accessibility", "units"
     )
 
 
-@shared_task
+@shared_task_email
 def import_division(name="import_division"):
     management.call_command("turku_services_import", "divisions")
 
 
-@shared_task
+@shared_task_email
 def import_all_addresses(name="import_all_addresses"):
     # Task that imports all the addresses and indexes search columns
     management.call_command("turku_services_import", "addresses")
@@ -33,54 +34,54 @@ def import_all_addresses(name="import_all_addresses"):
     management.call_command("index_search_columns")
 
 
-@shared_task
+@shared_task_email
 def import_addresses(name="import_addresses"):
     # Imports addresses for Turku and Karina from the WFS server hosted by Turku
     management.call_command("turku_services_import", "addresses")
 
 
-@shared_task
+@shared_task_email
 def geo_import_municipalities(name="geo_import_municipalities"):
     management.call_command("geo_import", "finland", "--municipalities")
 
 
-@shared_task
+@shared_task_email
 def index_search_columns(name="index_search_columns"):
     management.call_command("index_search_columns")
 
 
-@shared_task
+@shared_task_email
 def import_geo_search_addresses(name="import_geo_search_addresses"):
     # Imports the addresses of Southwest Finland(not Turku and Kaarina) from geo-search(paikkatietohaku)
     management.call_command("turku_services_import", "geo_search_addresses")
 
 
-@shared_task
+@shared_task_email
 def import_enriched_addresses(name="import_enriched_addresses"):
     # Enriches addresses for Turku and Karina from geo-search(paikkatietohaku)
     management.call_command("turku_services_import", "enriched_addresses")
 
 
-@shared_task
+@shared_task_email
 def import_bicycle_stands(name="import_bicycle_stands"):
     management.call_command("turku_services_import", "bicycle_stands")
 
 
-@shared_task
+@shared_task_email
 def import_bike_service_stations(name="bike_service_stations"):
     management.call_command("turku_services_import", "bike_service_stations")
 
 
-@shared_task
+@shared_task_email
 def import_gas_filling_stations(name="import_gas_filling_stations"):
     management.call_command("turku_services_import", "gas_filling_stations")
 
 
-@shared_task
+@shared_task_email
 def import_charging_stations(name="import_charging_stations"):
     management.call_command("turku_services_import", "charging_stations")
 
 
-@shared_task
+@shared_task_email
 def import_external_sources(name="import_external_sources"):
     management.call_command("turku_services_import", "external_sources")
