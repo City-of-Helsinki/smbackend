a83518216d6860198221057dd8e88edfe5a287ce
juuso-j <juuso.jokiniemi@turku.fi>
2023-02-24 13:25:41 +0200

    Add administrative division fixtures

--- street_maintenance/tests/conftest.py
+++ street_maintenance/tests/conftest.py
@@ -2,9 +2,15 @@ from datetime import datetime, timedelta
 
 import pytest
 import pytz
-from django.contrib.gis.geos import LineString
+from django.contrib.gis.geos import GEOSGeometry, LineString
+from munigeo.models import (
+    AdministrativeDivision,
+    AdministrativeDivisionGeometry,
+    AdministrativeDivisionType,
+)
 from rest_framework.test import APIClient
 
+from mobility_data.tests.conftest import TURKU_WKT
 from street_maintenance.management.commands.constants import (
     AURAUS,
     INFRAROAD,
@@ -69,5 +75,31 @@ def geometry_historys():
         provider=KUNTEC,
         events=[AURAUS, LIUKKAUDENTORJUNTA],
     )
-    geometry_historys.append(obj)
-    return geometry_historys
+    geometry_historys.append(obj) @ pytest.mark.django_db
+
+
+@pytest.fixture
+def administrative_division_type():
+    adm_div_type = AdministrativeDivisionType.objects.create(
+        id=1, type="muni", name="Municipality"
+    )
+    return adm_div_type
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def administrative_division(administrative_division_type):
+    adm_div = AdministrativeDivision.objects.get_or_create(
+        id=1, name="Turku", origin_id=853, type_id=1
+    )
+    return adm_div
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def administrative_division_geometry(administrative_division):
+    turku_multipoly = GEOSGeometry(TURKU_WKT, srid=3067)
+    adm_div_geom = AdministrativeDivisionGeometry.objects.create(
+        id=1, division_id=1, boundary=turku_multipoly
+    )
+    return adm_div_geom
