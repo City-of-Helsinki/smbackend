ef67c2fec0c6d9989f10d1f6c0e5443fe24f57ad
juuso-j <juuso.jokiniemi@turku.fi>
2023-02-27 10:38:41 +0200

    Add Kuntec tests

--- street_maintenance/tests/test_importers.py
+++ street_maintenance/tests/test_importers.py
@@ -1,58 +1,58 @@
-from datetime import datetime
 from unittest.mock import patch
 
 import pytest
 
-from street_maintenance.management.commands.constants import DATE_FORMATS, INFRAROAD
+from street_maintenance.management.commands.constants import INFRAROAD
 from street_maintenance.models import MaintenanceUnit, MaintenanceWork
 
-
-def get_infraroad_works_json_data(num_elements):
-    current_date = datetime.now().date().strftime(DATE_FORMATS[INFRAROAD])
-    location_history = [
-        {
-            "timestamp": f"{current_date} 08:29:49",
-            "coords": "(22.24957474 60.49515401)",
-            "events": ["au"],
-        },
-        {
-            "timestamp": f"{current_date} 08:29:28",
-            "coords": "(22.24946401 60.49515848)",
-            "events": ["au"],
-        },
-        {
-            "timestamp": f"{current_date} 08:28:32",
-            "coords": "(22.24944127 60.49519463)",
-            "events": ["hiekoitus"],
-        },
-    ]
-    assert num_elements <= len(location_history)
-    data = {"location_history": location_history[:num_elements]}
-    return data
+from .utils import (
+    get_infraroad_units_json_data,
+    get_infraroad_works_json_data,
+    get_kuntec_units_json_data,
+    get_kuntec_works_json_data,
+)
 
 
-def get_infraroad_units_json_data(num_elements):
-    current_date = datetime.now().date().strftime(DATE_FORMATS[INFRAROAD])
-    data = [
-        {
-            "id": 2817625,
-            "last_location": {
-                "timestamp": f"{current_date} 06:31:34",
-                "coords": "(22.249642023816705 60.49569119699299)",
-                "events": ["au"],
-            },
-        },
-        {
-            "id": 12891825,
-            "last_location": {
-                "timestamp": f"{current_date} 08:29:49",
-                "coords": "(22.24957474 60.49515401)",
-                "events": ["Kenttien hoito"],
-            },
-        },
-    ]
-    assert num_elements <= len(data)
-    return data[:num_elements]
+@pytest.mark.django_db
+@patch("street_maintenance.management.commands.utils.get_json_data")
+def test_kuntec(
+    get_json_data_mock, administrative_division, administrative_division_geometry
+):
+    from street_maintenance.management.commands.utils import (
+        create_kuntec_maintenance_units,
+        create_kuntec_maintenance_works,
+    )
+
+    # Note, the fixture JSON contains one unit item with IO_DIN state 0(off)
+    # i.e., will not be included
+    get_json_data_mock.return_value = get_kuntec_units_json_data(2)
+    num_created_units, num_del_units = create_kuntec_maintenance_units()
+    assert num_created_units == 2
+    assert num_del_units == 0
+    assert MaintenanceUnit.objects.count() == 2
+    unit = MaintenanceUnit.objects.first()
+    assert unit.unit_id == "150635"
+    assert unit.names == ["Auraus"]
+    get_json_data_mock.return_value = get_kuntec_units_json_data(1)
+    num_created_units, num_del_units = create_kuntec_maintenance_units()
+    assert unit.id == MaintenanceUnit.objects.first().id
+    assert num_created_units == 0
+    assert num_del_units == 1
+    assert MaintenanceUnit.objects.count() == 1
+    get_json_data_mock.return_value = get_kuntec_works_json_data(2)
+    num_created_works, num_del_works = create_kuntec_maintenance_works(3)
+    assert num_created_works == 2
+    assert num_del_works == 0
+    assert MaintenanceWork.objects.count() == 2
+    work = MaintenanceWork.objects.first()
+    work.events = ["auraus"]
+    work.original_event_names = ["Auraus"]
+    get_json_data_mock.return_value = get_kuntec_works_json_data(1)
+    num_created_works, num_del_works = create_kuntec_maintenance_works(3)
+    assert num_created_works == 0
+    assert num_del_works == 1
+    assert work.id == MaintenanceWork.objects.first().id
+    assert MaintenanceWork.objects.count() == 1
 
 
 @pytest.mark.django_db
