9da81862f72e005fb213178d193625e3f3eca61b
juuso-j <juuso.jokiniemi@turku.fi>
2023-03-07 13:25:23 +0200

    Test created ContentType field. change name to type_name

--- mobility_data/tests/test_import_bike_service_stations.py
+++ mobility_data/tests/test_import_bike_service_stations.py
@@ -1,6 +1,7 @@
 import pytest
 
 from mobility_data.importers.bike_service_stations import CONTENT_TYPE_NAME
+from mobility_data.importers.utils import get_content_type_config
 from mobility_data.models import ContentType, MobileUnit
 
 from .utils import import_command
@@ -11,8 +12,16 @@ def test_import_bike_service_stations():
     import_command(
         "import_bike_service_stations", test_mode="bike_service_stations.geojson"
     )
-    assert ContentType.objects.filter(name=CONTENT_TYPE_NAME).count() == 1
-    assert MobileUnit.objects.filter(content_types__name=CONTENT_TYPE_NAME).count() == 3
+    assert ContentType.objects.filter(type_name=CONTENT_TYPE_NAME).count() == 1
+    assert (
+        MobileUnit.objects.filter(content_types__type_name=CONTENT_TYPE_NAME).count()
+        == 3
+    )
+    config = get_content_type_config(CONTENT_TYPE_NAME)
+    content_type = ContentType.objects.get(type_name=CONTENT_TYPE_NAME)
+    content_type.name_fi = config["name"]["fi"]
+    content_type.name_sv = config["name"]["sv"]
+    content_type.name_en = config["name"]["en"]
     kupittaankentta = MobileUnit.objects.get(name="Kupittaankenttä")
     assert kupittaankentta.name_sv == "Kuppisplan"
     assert kupittaankentta.name_en == "Kupittaa court"
@@ -31,3 +40,12 @@ def test_import_bike_service_stations():
     assert roola.name_sv == "Röölä"
     assert roola.name_en == "Röölä"
     assert roola.extra["in_terrain"] == "Kyllä"
+    # Test that dublicates are not created
+    import_command(
+        "import_bike_service_stations", test_mode="bike_service_stations.geojson"
+    )
+    assert ContentType.objects.filter(type_name=CONTENT_TYPE_NAME).count() == 1
+    assert (
+        MobileUnit.objects.filter(content_types__type_name=CONTENT_TYPE_NAME).count()
+        == 3
+    )
