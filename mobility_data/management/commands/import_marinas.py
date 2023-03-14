4c80a203c428add6830834ce127e5d03f7c3dc96
juuso-j <juuso.jokiniemi@turku.fi>
2023-03-14 10:54:47 +0200

    Use generic functions to save and display logging message

--- mobility_data/management/commands/import_marinas.py
+++ mobility_data/management/commands/import_marinas.py
@@ -1,8 +1,17 @@
 import logging
 
 from mobility_data.importers.marinas import (
-    import_guest_marina_and_boat_parking,
-    import_marinas,
+    BOAT_PARKING_CONTENT_TYPE_NAME,
+    get_boat_parkings,
+    get_guest_marinas,
+    get_marinas,
+    GUEST_MARINA_CONTENT_TYPE_NAME,
+    MARINA_CONTENT_TYPE_NAME,
+)
+from mobility_data.importers.utils import (
+    get_or_create_content_type_from_config,
+    log_imported_message,
+    save_to_database,
 )
 
 from ._base_import_command import BaseImportCommand
@@ -12,6 +21,21 @@ logger = logging.getLogger("mobility_data")
 
 class Command(BaseImportCommand):
     def handle(self, *args, **options):
-        logger.info(f"Imported {import_marinas()} marinas.")
-        import_guest_marina_and_boat_parking()
-        logger.info("Imported guest marina and boat parking.")
+        objects = get_marinas()
+        content_type = get_or_create_content_type_from_config(MARINA_CONTENT_TYPE_NAME)
+        num_ceated, num_deleted = save_to_database(objects, content_type)
+        log_imported_message(logger, content_type, num_ceated, num_deleted)
+
+        objects = get_boat_parkings()
+        content_type = get_or_create_content_type_from_config(
+            BOAT_PARKING_CONTENT_TYPE_NAME
+        )
+        num_ceated, num_deleted = save_to_database(objects, content_type)
+        log_imported_message(logger, content_type, num_ceated, num_deleted)
+
+        objects = get_guest_marinas()
+        content_type = get_or_create_content_type_from_config(
+            GUEST_MARINA_CONTENT_TYPE_NAME
+        )
+        num_ceated, num_deleted = save_to_database(objects, content_type)
+        log_imported_message(logger, content_type, num_ceated, num_deleted)
