1e49dd0c0f83c4bab6e9f5c55dba4e984d8f5cdd
juuso-j <juuso.jokiniemi@turku.fi>
2023-10-16 11:45:32 +0300

    Improve naming and add descriptions

--- environment_data/management/commands/weather_observation_constants.py
+++ environment_data/management/commands/weather_observation_constants.py
@@ -1,21 +1,23 @@
 # Note, change to 2010
 START_YEAR = 2022
 STATION_MATCH_STRINGS = ["Automaattinen sääasema"]
-TEMPERATURE = "Temperature"
-PRECIPITATIONAMOUNT = "PrecipitationAmount"
-HUMINIDITY = "Humidity"
-PRESSURE = "Pressure"
-WINDDIRETION = "WindDirection"
-WINDSPEEDMS = "WindSpeedMS"
-OBSERVABLE_PARAMETERS = [
-    TEMPERATURE,
-    PRECIPITATIONAMOUNT,
-    HUMINIDITY,
-    PRESSURE,
-    WINDDIRETION,
-    WINDSPEEDMS,
-]
-PARAMS = {
+AIR_TEMPERATURE = "TA_PT1H_AVG"
+RELATIVE_HUMIDITY = "RH_PT1H_AVG"
+WIND_SPEED = "WS_PT1H_AVG"
+WIND_DIRECTION = "WD_PT1H_AVG"
+PRECIPITATION_AMOUNT = "PRA_PT1H_ACC"
+AIR_PRESSURE = ""
+
+OBSERVABLE_PARAMETERS = [AIR_TEMPERATURE, RELATIVE_HUMIDITY, WIND_SPEED]
+PARAMETER_DESCRIPTIONS = {
+    AIR_TEMPERATURE: "Air temperature - degC",
+    RELATIVE_HUMIDITY: "Relative humidity - %",
+    WIND_SPEED: "Wind speed - m/s",
+    WIND_DIRECTION: "Wind direction - deg",
+    PRECIPITATION_AMOUNT: "Precipitation amount - mm",
+    AIR_PRESSURE: "Air pressure - hPA",
+}
+REQUEST_PARAMS = {
     "service": "WFS",
     "version": "2.0.0",
     "request": "getFeature",
@@ -29,10 +31,11 @@ PARAMS = {
 }
 DATA_URL = "https://opendata.fmi.fi/wfs"
 """
-https://opendata.fmi.fi/wfs/fin?service=WFS&version=2.0.0&request=GetFeature&storedquery_id=
-fmi::observations::weather::hourly::timevaluepair&fmisid=100949&timestep=60&parameters=
-Temperature,pressure&startTime=2023-10-9T00:00:00Z&endTime=2023-10-10T23:00:00Z
--100908
+https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi%3A%3Aobservations%3A%3Aweather%3A%3Ahourly%3A%3Atimevaluepair&
+fmisid=100908&startTime=2022-1-01T00%3A00Z&endTime=2022-1-31T23%3A00Z&timeStep=60
+https://opendata.fmi.fi/meta?observableProperty=observation&param=TA_PT1H_MAX&language=eng
+https://opendata.fmi.fi/meta?observableProperty=observation&param=WS_PT1H_MAX&language=eng
+https://opendata.fmi.fi/meta?observableProperty=observation&param=TA_PT1H_MIN&language=eng
 https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=describeStoredQueries&
 Pressure,GeopHeight,Temperature,DewPoint,Humidity,WindDirection, WindSpeedMS,WindUMS,
 WindVMS,PrecipitationAmount,TotalCloudCover,LowCloudCover, MediumCloudCover,HighCloudCover,
