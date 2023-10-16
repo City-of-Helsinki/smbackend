d6e0cb978eb889cc413e18769af068e8340c7e6c
juuso-j <juuso.jokiniemi@turku.fi>
2023-10-16 11:44:08 +0300

    Improve naming and add descriptions

--- environment_data/management/commands/air_quality_constants.py
+++ environment_data/management/commands/air_quality_constants.py
@@ -1,22 +1,31 @@
 START_YEAR = 2010
 # NOTE, No more than 10000 hours is allowed per request.
-AQINDEX_PT1H_AVG = "AQINDEX_PT1H_avg"  # Ilmanlaatuindeksi
-PM10_PT1H_AVG = "PM10_PT1H_avg"  # Hengitettävät hiukkaset
-SO2_PT1H_AVG = "SO2_PT1H_avg"  # rikkidioksiidi
-O3_PT1H_avg = "O3_PT1H_avg"  # otsooni
-PM25_PT1H_avg = "PM25_PT1H_avg"  # pienhiukkaset
-NO2_PT1H_avg = "NO2_PT1H_avg"  # typpidioksiidi
+AIR_QUALITY_INDEX = "AQINDEX_PT1H_avg"  # Ilmanlaatuindeksi
+PARTICULATE_MATTER_10 = "PM10_PT1H_avg"  # Hengitettävät hiukkaset
+SULPHUR_DIOXIDE = "SO2_PT1H_avg"  # rikkidioksiidi
+OZONE = "O3_PT1H_avg"  # otsooni
+PARTICULATE_MATTER_25 = "PM25_PT1H_avg"  # pienhiukkaset
+NITROGEN_DIOXIDE = "NO2_PT1H_avg"  # typpidioksiidi
 
 OBSERVABLE_PARAMETERS = [
-    AQINDEX_PT1H_AVG,
-    PM10_PT1H_AVG,
-    SO2_PT1H_AVG,
-    O3_PT1H_avg,
-    PM25_PT1H_avg,
-    NO2_PT1H_avg,
+    AIR_QUALITY_INDEX,
+    PARTICULATE_MATTER_10,
+    SULPHUR_DIOXIDE,
+    OZONE,
+    PARTICULATE_MATTER_25,
+    NITROGEN_DIOXIDE,
 ]
+PARAMETER_DESCRIPTIONS = {
+    AIR_QUALITY_INDEX: "Air quality index",
+    SULPHUR_DIOXIDE: "Sulphur dioxide - ug/m3",
+    NITROGEN_DIOXIDE: "Nitrogen dioxide - ug/m3",
+    OZONE: "Ozone - ug/m3",
+    PARTICULATE_MATTER_10: "Particulate matter < 10 µm - ug/m3",
+    PARTICULATE_MATTER_25: "Particulate matter < 2.5 µm - ug/m3",
+}
 
-PARAMS = {
+
+REQUEST_PARAMS = {
     "request": "getFeature",
     "storedquery_id": "urban::observations::airquality::hourly::timevaluepair",
     "geoId": None,
