e2b742a51e86431bba43c98350153138ec691dd3
juuso-j <juuso.jokiniemi@turku.fi>
2023-10-17 14:22:51 +0300

    Fix OBSERVABLE_PARAMETERS and AIR_PRESSURE

--- environment_data/management/commands/weather_observation_constants.py
+++ environment_data/management/commands/weather_observation_constants.py
@@ -1,14 +1,20 @@
-# Note, change to 2010
-START_YEAR = 2022
+START_YEAR = 2010
 STATION_MATCH_STRINGS = ["Automaattinen sääasema"]
 AIR_TEMPERATURE = "TA_PT1H_AVG"
 RELATIVE_HUMIDITY = "RH_PT1H_AVG"
 WIND_SPEED = "WS_PT1H_AVG"
 WIND_DIRECTION = "WD_PT1H_AVG"
 PRECIPITATION_AMOUNT = "PRA_PT1H_ACC"
-AIR_PRESSURE = ""
+AIR_PRESSURE = "PA_PT1H_AVG"
 
-OBSERVABLE_PARAMETERS = [AIR_TEMPERATURE, RELATIVE_HUMIDITY, WIND_SPEED]
+OBSERVABLE_PARAMETERS = [
+    AIR_TEMPERATURE,
+    RELATIVE_HUMIDITY,
+    WIND_SPEED,
+    WIND_DIRECTION,
+    PRECIPITATION_AMOUNT,
+    AIR_PRESSURE,
+]
 PARAMETER_DESCRIPTIONS = {
     AIR_TEMPERATURE: "Air temperature - degC",
     RELATIVE_HUMIDITY: "Relative humidity - %",
@@ -23,22 +29,19 @@ REQUEST_PARAMS = {
     "request": "getFeature",
     "storedquery_id": "fmi::observations::weather::hourly::timevaluepair",
     "fmisid": None,
-    # "geoId": None,
     "parameters": None,
     "startTime": None,
     "endTime": None,
     "timeStep": 60,
 }
+# Note no more than 744 hours is allowed per request
 DATA_URL = "https://opendata.fmi.fi/wfs"
+
 """
-https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi%3A%3Aobservations%3A%3Aweather%3A%3Ahourly%3A%3Atimevaluepair&
+Obtaining the paramter info
+Go to url:
+https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=
+fmi%3A%3Aobservations%3A%3Aweather%3A%3Ahourly%3A%3Atimevaluepair&
 fmisid=100908&startTime=2022-1-01T00%3A00Z&endTime=2022-1-31T23%3A00Z&timeStep=60
-https://opendata.fmi.fi/meta?observableProperty=observation&param=TA_PT1H_MAX&language=eng
-https://opendata.fmi.fi/meta?observableProperty=observation&param=WS_PT1H_MAX&language=eng
-https://opendata.fmi.fi/meta?observableProperty=observation&param=TA_PT1H_MIN&language=eng
-https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=describeStoredQueries&
-Pressure,GeopHeight,Temperature,DewPoint,Humidity,WindDirection, WindSpeedMS,WindUMS,
-WindVMS,PrecipitationAmount,TotalCloudCover,LowCloudCover, MediumCloudCover,HighCloudCover,
-RadiationGlobal,RadiationGlobalAccumulation, RadiationNetSurfaceLWAccumulation,RadiationNetSurfaceSWAccumulation,
- RadiationSWAccumulation,Visibility,WindGust,Cape
+Find observedProperty elements and go to the link defined in xlink:href
 """
