664141f947f318c75f8da3d1aa21e8eb798eae0b
juuso-j <juuso.jokiniemi@turku.fi>
2023-10-18 10:52:54 +0300

    Add REQUEST_SESSION and retry_strategy

--- environment_data/constants.py
+++ environment_data/constants.py
@@ -1,5 +1,9 @@
 import types
 
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
 AIR_QUALITY = "AQ"
 WEATHER_OBSERVATION = "WO"
 DATA_TYPES_FULL_NAME = {
@@ -19,3 +23,14 @@ VALID_DATA_TYPE_CHOICES = ", ".join(
     [item[0] + f" ({item[1]})" for item in DATA_TYPES_FULL_NAME.items()]
 )
 DATA_TYPES_LIST = [AIR_QUALITY, WEATHER_OBSERVATION]
+
+retry_strategy = Retry(
+    total=10,
+    status_forcelist=[429],
+    allowed_methods=["GET", "POST"],
+    backoff_factor=30,  # 30, 60, 120 , 240, ..seconds
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+REQUEST_SESSION = requests.Session()
+REQUEST_SESSION.mount("https://", adapter)
+REQUEST_SESSION.mount("http://", adapter)
