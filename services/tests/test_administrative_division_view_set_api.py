87bb271002f71906c53a91cf9099afdad7419f26
Pauliina Ilmanen <pauliina.jalava@anders.fi>
2023-11-01 16:26:15 +0200

    Add test for municipality filter in Administrative Division endpoint

--- services/tests/test_administrative_division_view_set_api.py
+++ services/tests/test_administrative_division_view_set_api.py
@@ -1,10 +1,29 @@
 import pytest
 from django.urls import reverse
-from munigeo.models import AdministrativeDivision, AdministrativeDivisionType
+from munigeo.models import (
+    AdministrativeDivision,
+    AdministrativeDivisionType,
+    Municipality,
+)
 from rest_framework.test import APIClient
 
 from services.api import make_muni_ocd_id
 from services.tests.utils import get
+
+
+def create_administrative_divisions():
+    municipality_ids = ["helsinki", "espoo", "vantaa"]
+    division_type = AdministrativeDivisionType.objects.create(type="muni")
+    for municipality_id in municipality_ids:
+        municipality = Municipality.objects.create(
+            id=municipality_id, name=municipality_id
+        )
+        AdministrativeDivision.objects.create(
+            type=division_type,
+            name=municipality_id,
+            ocd_id=make_muni_ocd_id(municipality_id),
+            municipality=municipality,
+        )
 
 
 @pytest.fixture
@@ -14,14 +33,20 @@ def api_client():
 
 @pytest.mark.django_db
 def test_get_administrative_division_list(api_client):
-    municipality_id = "helsinki"
-    division_type = AdministrativeDivisionType.objects.create(type="muni")
-    AdministrativeDivision.objects.create(
-        type=division_type,
-        name=municipality_id,
-        ocd_id=make_muni_ocd_id(municipality_id),
-    )
-
+    create_administrative_divisions()
     response = get(api_client, reverse("administrativedivision-list"))
     assert response.status_code == 200
+    assert response.data["count"] == 3
+
+
+@pytest.mark.django_db
+def test_municipality_filter(api_client):
+    create_administrative_divisions()
+    response = get(
+        api_client,
+        reverse("administrativedivision-list"),
+        data={"municipality": "helsinki"},
+    )
+    assert response.status_code == 200
     assert response.data["count"] == 1
+    assert response.data["results"][0]["municipality"] == "helsinki"
