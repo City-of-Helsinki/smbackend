fdeceba982c5ea0385734e54c8c48e2bf361945d
Pauliina Ilmanen <pauliina.jalava@anders.fi>
2024-01-12 11:13:13 +0200

    Update setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="smbackend",
-    version="230904",
+    version="240115",
     license="AGPLv3",
     packages=find_packages(),
     include_package_data=True,
