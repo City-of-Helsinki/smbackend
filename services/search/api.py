eea1014bf81615b9306be3c5a7d10137516e38c0
Pauliina Ilmanen <pauliina.jalava@anders.fi>
2024-03-11 09:30:49 +0200

    Accept dot and apostrophe in search validation

--- services/search/api.py
+++ services/search/api.py
@@ -343,9 +343,9 @@ class SearchViewSet(GenericAPIView):
         if not q_val:
             raise ParseError("Supply search terms with 'q=' ' or input=' '")
 
-        if not re.match(r"^[\w\såäö+&|-]+$", q_val):
+        if not re.match(r"^[\w\såäö.'+&|-]+$", q_val):
             raise ParseError(
-                "Invalid search terms, only letters, numbers, spaces and +-&| allowed."
+                "Invalid search terms, only letters, numbers, spaces and .'+-&| allowed."
             )
 
         types_str = ",".join([elem for elem in QUERY_PARAM_TYPE_NAMES])
