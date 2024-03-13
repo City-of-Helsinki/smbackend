32d4453d7c0b04b023635e352af003d9cbfcd371
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-03-13 13:46:51 +0200

    Add is_xml and XML validation

--- iot/models.py
+++ iot/models.py
@@ -1,6 +1,8 @@
 import json
+from xml.parsers.expat import ExpatError
 
 import requests
+import xmltodict
 from django.core.exceptions import ValidationError
 from django.db import models
 
@@ -13,6 +15,9 @@ class IoTDataSource(models.Model):
         "Set the identifier as an argument to the Celery task that fetches the data.",
     )
     source_full_name = models.CharField(max_length=64, null=True)
+    is_xml = models.BooleanField(
+        default=False, verbose_name="If True, XML data will be converted to JSON."
+    )
     url = models.URLField()
     headers = models.JSONField(
         null=True,
@@ -26,16 +31,26 @@ class IoTDataSource(models.Model):
     def clean(self):
         # Test if url exists
         try:
-            response = requests.get(self.url)
+            response = requests.get(self.url, headers=self.headers)
         except requests.exceptions.ConnectionError:
             raise ValidationError(f"The given url {self.url} does not exist.")
-        # Test if valid json
-        try:
-            response.json()
-        except json.decoder.JSONDecodeError:
-            raise ValidationError(
-                f"Could not parse the JSON data from the given url {self.url}"
-            )
+
+        # Test if XML data can be parsed into JSON
+        if self.is_xml:
+            try:
+                xmltodict.parse(response.text)
+            except ExpatError as err:
+                raise ExpatError(
+                    f"Could not parse XML data from the give url {self.url}. {err}"
+                )
+        else:
+            # Test if valid JSON
+            try:
+                response.json()
+            except json.decoder.JSONDecodeError as err:
+                raise ValidationError(
+                    f"Could not parse the JSON data from the given url {self.url}. {err}"
+                )
 
 
 class IoTData(models.Model):
