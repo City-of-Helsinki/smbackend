fe80936026076f4dd197a9bc3a9c8109075c1f23
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-03-26 08:30:03 +0200

    Add filters SituationViewSet

--- exceptional_situations/api/views.py
+++ exceptional_situations/api/views.py
@@ -1,3 +1,5 @@
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from exceptional_situations.api.serializers import (
@@ -14,9 +16,60 @@ from exceptional_situations.models import (
 )
 
 
+class SituationFilter(django_filters.FilterSet):
+    is_active = django_filters.BooleanFilter(method="filter_is_active")
+    situation_type_str = django_filters.CharFilter(method="filter_situation_type_str")
+    start_time__gt = django_filters.DateTimeFilter(method="filter_start_time__gt")
+    start_time__lt = django_filters.DateTimeFilter(method="filter_start_time__lt")
+    end_time__gt = django_filters.DateTimeFilter(method="filter_end_time__gt")
+    end_time__lt = django_filters.DateTimeFilter(method="filter_end_time__lt")
+
+    class Meta:
+        model = Situation
+        fields = {
+            "situation_type": ["exact"],
+            "situation_id": ["exact"],
+            "release_time": ["lt", "gt"],
+        }
+
+    def filter_situation_type_str(self, queryset, fields, situation_type_str):
+        ids = [
+            obj.id for obj in queryset if obj.situation_type_str == situation_type_str
+        ]
+        return queryset.filter(id__in=ids)
+
+    def filter_is_active(self, queryset, fields, active):
+        ids = [obj.id for obj in queryset if obj.is_active == bool(active)]
+        return queryset.filter(id__in=ids)
+
+    def filter_start_time__gt(self, queryset, fields, start_time):
+        ids = [obj.id for obj in queryset if obj.start_time > start_time]
+        return queryset.filter(id__in=ids)
+
+    def filter_start_time__lt(self, queryset, fields, start_time):
+        ids = [obj.id for obj in queryset if obj.start_time < start_time]
+        return queryset.filter(id__in=ids)
+
+    def filter_end_time__gt(self, queryset, fields, start_time):
+        ids = [obj.id for obj in queryset if obj.start_time > start_time]
+        return queryset.filter(id__in=ids)
+
+    def filter_end_time__lt(self, queryset, fields, start_time):
+        ids = [obj.id for obj in queryset if obj.start_time < start_time]
+        return queryset.filter(id__in=ids)
+
+
 class SituationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Situation.objects.all()
     serializer_class = SituationSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = SituationFilter
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.queryset)
+        page = self.paginate_queryset(queryset)
+        serializer = self.serializer_class(page, many=True)
+        return self.get_paginated_response(serializer.data)
 
 
 class SituationLocationViewSet(viewsets.ReadOnlyModelViewSet):
