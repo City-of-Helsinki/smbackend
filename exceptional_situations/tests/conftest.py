ca79b5db1549123142a3bf73e50640abfc57007c
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-03-27 12:02:01 +0200

    Add test fixtures

---
+++ exceptional_situations/tests/conftest.py
@@ -0,0 +1,114 @@
+from datetime import datetime, timedelta
+
+import pytest
+from django.contrib.gis.geos import GEOSGeometry
+from rest_framework.test import APIClient
+
+from exceptional_situations.models import (
+    Situation,
+    SituationAnnouncement,
+    SituationLocation,
+    SituationType,
+)
+
+NOW = datetime.now()
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def situation_types():
+    SituationType.objects.create(
+        type_name="test type name", sub_type_name="test sub type name"
+    )
+    return SituationType.objects.all()
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def locations():
+    json_data = {"test_key": "test_value"}
+    SituationLocation.objects.create(
+        details=json_data, geometry=GEOSGeometry("POINT(0 0)")
+    )
+    SituationLocation.objects.create(
+        details=json_data, geometry=GEOSGeometry("POINT(1 0)")
+    )
+    SituationLocation.objects.create(
+        details=json_data, geometry=GEOSGeometry("POINT(0 1)")
+    )
+
+    return SituationLocation.objects.all()
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def announcements(locations):
+    json_data = {"test_key": "test_value"}
+    SituationAnnouncement.objects.create(
+        title="two hours",
+        description="two hours long situation",
+        additional_info=json_data,
+        location=locations[0],
+        start_time=NOW - timedelta(hours=1),
+        end_time=NOW + timedelta(hours=1),
+    )
+    SituationAnnouncement.objects.create(
+        title="two days",
+        description="two days long situation",
+        additional_info=json_data,
+        location=locations[1],
+        start_time=NOW - timedelta(days=1),
+        end_time=NOW + timedelta(days=1),
+    )
+
+    return SituationAnnouncement.objects.all()
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def inactive_announcements(locations):
+    json_data = {"test_key": "test_value"}
+    SituationAnnouncement.objects.create(
+        title="in past",
+        description="inactive announcement",
+        additional_info=json_data,
+        location=locations[2],
+        start_time=NOW - timedelta(days=2),
+        end_time=NOW - timedelta(days=1),
+    )
+    return SituationAnnouncement.objects.all()
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def inactive_situations(situation_types, inactive_announcements):
+    situation = Situation.objects.create(
+        release_time=NOW,
+        situation_id="inactive",
+        situation_type=situation_types.first(),
+    )
+    situation.announcements.add(inactive_announcements.first())
+    return Situation.objects.all()
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def situations(situation_types, announcements):
+    situation = Situation.objects.create(
+        release_time=NOW,
+        situation_id="TwoHoursLong",
+        situation_type=situation_types.first(),
+    )
+    situation.announcements.add(announcements[0])
+    situation = Situation.objects.create(
+        release_time=NOW - timedelta(days=1),
+        situation_id="TwoDaysLong",
+        situation_type=situation_types.first(),
+    )
+    situation.announcements.add(announcements[1])
+    return Situation.objects.all()
