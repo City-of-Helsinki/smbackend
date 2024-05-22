bb47ff6c9096315e67d9bf58076cb8ba454c8eae
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-05-22 09:43:34 +0300

    Fix faulty assertion

--- mobility_data/tests/test_api.py
+++ mobility_data/tests/test_api.py
@@ -1,6 +1,4 @@
 import pytest
-from django.conf import settings
-from django.contrib.gis.geos import Point
 from rest_framework.reverse import reverse
 
 
@@ -47,8 +45,8 @@ def test_mobile_unit(api_client, mobile_units, content_types, unit):
     assert result["extra"]["test_string"] == "4242"
     assert result["extra"]["test_int"] == 4242
     assert result["extra"]["test_float"] == 42.42
-    assert result["geometry"] == Point(
-        235404.6706163187, 6694437.919005549, srid=settings.DEFAULT_SRID
+    assert (
+        result["geometry"] == "SRID=3067;POINT (235404.67061631865 6694437.919005549)"
     )
     url = reverse(
         "mobility_data:mobile_units-detail",
