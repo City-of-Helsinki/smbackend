3812bf4c916b5921464feb23e678bbe1fc768cdb
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-06-25 13:42:40 +0300

    Update GML fields

--- mobility_data/importers/bicycle_stands.py
+++ mobility_data/importers/bicycle_stands.py
@@ -148,16 +148,18 @@ class BicyleStand(MobileUnitDataBase):
             self.extra["model"] = model_elem.as_string()
         else:
             self.extra["model"] = None
-        num_stands_elem = feature["Lukumaara"]
-        if num_stands_elem is not None:
-            num = num_stands_elem.as_int()
-            # for bicycle stands that are Not maintained by Turku
-            # the number of stands is set to 0 in the input data
-            # but in reality there is no data so None is set.
-            if num == 0 and not self.extra["maintained_by_turku"]:
-                self.extra["number_of_stands"] = None
-            else:
-                self.extra["number_of_stands"] = num
+
+        # For some reason the field "lukumaara" has been removed
+        # num_stands_elem = feature["lukumaara"]
+        # if num_stands_elem is not None:
+        #     num = num_stands_elem.as_int()
+        #     # for bicycle stands that are Not maintained by Turku
+        #     # the number of stands is set to 0 in the input data
+        #     # but in reality there is no data so None is set.
+        #     if num == 0 and not self.extra["maintained_by_turku"]:
+        #         self.extra["number_of_stands"] = None
+        #     else:
+        #         self.extra["number_of_stands"] = num
 
         num_places_elem = feature["Pyorapaikkojen_lukumaara"].as_string()
         if num_places_elem:
@@ -219,7 +221,12 @@ def get_bicycle_stand_objects():
         for feature in data_source[1][0]:
             bicycle_stand = BicyleStand()
             if data_source[0] == "gml":
-                bicycle_stand.set_gml_feature(feature)
+                try:
+                    bicycle_stand.set_gml_feature(feature)
+                except IndexError as err:
+                    # Handle IndexError gracefully, as the field names can change in the source data
+                    # and it would prevent importing the GEOJSON data.
+                    logger.warning(f"Discarding {feature}, reason: {err}")
             elif data_source[0] == "geojson":
                 bicycle_stand.set_geojson_feature(feature)
             if (
