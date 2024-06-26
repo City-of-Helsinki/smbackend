3bbb23ecfa4b9f0282342008d1d989debb9693f7
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-06-26 11:48:49 +0300

    Add prefix "Bicycle parking" to names

--- mobility_data/importers/bicycle_stands.py
+++ mobility_data/importers/bicycle_stands.py
@@ -56,7 +56,6 @@ class BicyleStand(MobileUnitDataBase):
 
     def __init__(self):
         super().__init__()
-        self.prefix_name = {}
         self.related_unit = None
         self.extra = {f: None for f in self.EXTRA_FIELDS}
 
@@ -110,7 +109,7 @@ class BicyleStand(MobileUnitDataBase):
         else:
             self.name["sv"] = name
             self.name["en"] = name
-        self.prefix_name = {k: f"{NAME_PREFIX[k]} {v}" for k, v in self.name.items()}
+        self.name = {k: f"{NAME_PREFIX[k]} {v}" for k, v in self.name.items()}
         addr = feature["osoite"].as_string().split(",")
         # Some addresses are in format:"Pyhän Henrikin aukio, Kupittaankatu 8, 20520 Turku"
         # Then remove the prefix part.
@@ -188,8 +187,8 @@ class BicyleStand(MobileUnitDataBase):
         full_names = get_closest_address_full_name(self.geometry)
         self.name[FI_KEY] = full_names[FI_KEY]
         self.name[SV_KEY] = full_names[SV_KEY]
-        self.name[EN_KEY] = full_names[EN_KEY]
-        self.prefix_name = {k: f"{NAME_PREFIX[k]} {v}" for k, v in self.name.items()}
+        self.name[EN_KEY] = full_names[FI_KEY]
+        self.name = {k: f"{NAME_PREFIX[k]} {v}" for k, v in self.name.items()}
 
 
 def get_data_sources():
