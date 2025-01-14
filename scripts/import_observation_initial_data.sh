4af4b58c9644befa32e1362dae562566cfd50633
Matti Eiden <matti.eiden@anders.com>
2025-01-14 13:24:26 +0200

    refactor: manual shellcheck fixes
    refs: PL-61

--- scripts/import_observation_initial_data.sh
+++ scripts/import_observation_initial_data.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-% export PYTHONIOENCODING=utf-8
+export PYTHONIOENCODING=utf-8
 set -e
 
 TIMESTAMP_FORMAT="+%Y-%m-%d %H:%M:%S"
 ROOT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-if [ -f $ROOT_PATH/local_update_config ]; then
-    $ROOT_PATH/local_update_config
+if [ -f "$ROOT_PATH/local_update_config" ]; then
+    "$ROOT_PATH/local_update_config"
 fi
 
 echo ---------------------------------
 echo "$(date "$TIMESTAMP_FORMAT") Importing observation initial data"
 echo ---------------------------------
 
-cd $ROOT_PATH
+cd "$ROOT_PATH"
 
 timeout 20m nice python manage.py loaddata observations/fixtures/maintenance_users.yaml 2>&1
 timeout 20m nice python manage.py loaddata observations/fixtures/initial_observable_properties_common.yaml 2>&1
