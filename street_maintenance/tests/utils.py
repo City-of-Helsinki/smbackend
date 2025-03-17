4eab1620599c8bda704a9803bb24a9ac983629b0
juuso-j <68938778+juuso-j@users.noreply.github.com>
2025-03-17 13:04:24 +0200

    Remove io_din for muu työ

--- street_maintenance/tests/utils.py
+++ street_maintenance/tests/utils.py
@@ -258,8 +258,9 @@ def get_kuntec_units_mock_data(num_elements):
             "created_at": "2019-11-05T10:10:38Z",
             "io_din": [
                 {"no": 1, "label": "Auraus", "state": 1},
-                {"no": 2, "label": "Hiekoitus", "state": 1},
-                {"no": 3, "label": "Muu ty\u00f6", "state": 0},
+                {"no": 2, "label": "Hiekoitus", "state": 0},
+                # Commented, as io_din state check removed in importer
+                # {"no": 3, "label": "Muu ty\u00f6", "state": 0},
             ],
         },
         {
