07165babbb42b5d5147a750deaaebb54df4bdf76
Terho <terho.craven@gofore.com>
2025-06-09 12:05:37 +0300

    Deploy code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 set -e
+
+cp -r data_from_github/* data
+
+# Enable SSH and give it access to app setting env variables
+if [[ "$ENABLE_SSH" = "true" ]]; then
+    service ssh start
+    eval $(printenv | sed -n "/^PWD=/!s/^\([^=-]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+fi
+
 if [[ "$APPLY_MIGRATIONS" = "true" ]]; then
     echo "Applying database migrations..."
     ./manage.py migrate --noinput
 fi
 
-if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
-    echo "Creating superuser if it does not exists."
-    python manage.py ensure_adminuser --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL \
-        --password $DJANGO_SUPERUSER_PASSWORD
-fi
+# if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+#     echo "Creating superuser if it does not exists."
+#     python manage.py ensure_adminuser --username $DJANGO_SUPERUSER_USERNAME \
+#         --email $DJANGO_SUPERUSER_EMAIL \
+#         --password $DJANGO_SUPERUSER_PASSWORD
+# fi
 
 if [ "$CELERY_BROKER_URL" ]; then
     echo "Starting celery worker detached..."
@@ -31,11 +40,12 @@ elif [ "$1" ]; then
     echo "Running command: $1"
     $1
 else
-    exec uwsgi --plugin http,python3 --master --http :8000 \
+    su -s /bin/bash nobody --command "exec uwsgi --plugin http,python3 --master --http :8000 \
                --processes 4 --threads 1 \
+               --static-map ${STATIC_URL}=${STATIC_ROOT} \
                --need-app \
                --mount ${URL_PREFIX:-/}=smbackend/wsgi.py \
                --manage-script-name \
                --die-on-term \
-               --strict
+               --strict"
 fi
