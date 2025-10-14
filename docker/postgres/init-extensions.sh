a203d22275958116aff0a249c7930bc82367a2f2
Mika Hietanen <m.hietanen84@gmail.com>
2025-10-14 17:08:29 +0300

    build: update docker compose setup
    Updates:
    - Update to use Debian PostGIS v17-image
    - Remove postgis-extension installation
    as it comes with the base image
    - Rename 'docker-entrypoint.sh' to 'init-extensions.sh'
    for an improved readability
    - Use own '.env'-file for Docker Compose
    - Upgrade/simplify Docker Compose settings
    - Remove UI-setup from Docker Compose
    - Update README
    
    Refs: PL-131

--- docker/postgres/init-extensions.sh
+++ docker/postgres/init-extensions.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-for ext in postgis hstore pg_trgm; do
+for ext in hstore pg_trgm; do
     create_ext_sql="CREATE EXTENSION IF NOT EXISTS $ext"
-    # Add the extensions to the default db template so that any
-    # new db will have the extensions enabled includin test db.
     echo "Installing $ext extension for template1"
     psql --username "$POSTGRES_USER" template1 -c "$create_ext_sql"
     echo "Installing $ext extension for $POSTGRES_DB database"
