2c65c8a32e0fc32dfd56b50e84af1743c01c0868
Nico Virkki <nico.virkki@anders.com>
2026-03-31 13:53:18 +0300

    feat: use django-helsinki-health-endpoints for readiness and healthz
    Make use of django-helsinki-health-endpoints library to publish
    readiness and healthz endpoints.
    
    Add SENTRY_RELEASE to django settings from env for the library.
    
    Refs: KEH-242

--- smbackend/tests/test_endpoints.py
+++ smbackend/tests/test_endpoints.py
@@ -2,13 +2,13 @@ from pytest import mark
 
 
 def test_healthz_endpoint(client):
-    response = client.get("/healthz/")
+    response = client.get("/healthz")
     assert response.status_code == 200
 
 
 @mark.django_db
 def test_readiness_endpoint(client):
-    response = client.get("/readiness/")
+    response = client.get("/readiness")
     assert response.status_code == 200
 
 
